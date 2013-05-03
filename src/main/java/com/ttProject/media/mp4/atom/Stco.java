6f48edc752919f59d76fe5ee59e9166d7b836fe9
Toda Takahiko <poepoemix@hotmail.com>
2013-05-03 09:38:48 +0900

    こっちもちょっと変更。 これでとりあえず、musicTubeM4aと同じ処理はできる基礎ができたと思う。

--- src/main/java/com/ttProject/media/mp4/atom/Stco.java
+++ src/main/java/com/ttProject/media/mp4/atom/Stco.java
@@ -1,19 +1,40 @@
 package com.ttProject.media.mp4.atom;
 
+import java.nio.ByteBuffer;
+
+import com.ttProject.library.BufferUtil;
 import com.ttProject.media.mp4.Atom;
 import com.ttProject.media.mp4.IAtomAnalyzer;
 import com.ttProject.nio.channels.IFileReadChannel;
 
 public class Stco extends Atom {
+	private byte version;
+	private int flags;
+	private int offsetCount;
 	public Stco(int size, int position) {
 		super(Stco.class.getSimpleName().toLowerCase(), size, position);
 	}
 	@Override
 	public void analyze(IFileReadChannel ch, IAtomAnalyzer analyzer) throws Exception {
-
+		ch.position(getPosition() + 8);
+		ByteBuffer buffer = BufferUtil.safeRead(ch, 8);
+		int head = buffer.getInt();
+		version = (byte)((head >> 24) & 0xFF);
+		flags = (head & 0x00FFFFFF);
+		offsetCount = buffer.getInt();
+		analyzed();
 	}
 	@Override
 	public String toString() {
 		return super.toString("          ");
+	}
+	public byte getVersion() {
+		return version;
+	}
+	public int getFlags() {
+		return flags;
+	}
+	public int getOffsetCount() {
+		return offsetCount;
 	}
 }
