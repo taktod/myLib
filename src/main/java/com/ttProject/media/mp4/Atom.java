9660410d72b8842b7d144517382aeb32a69c97ea
Toda Takahiko <poepoemix@hotmail.com>
2013-05-04 09:12:17 +0900

    コピー動作をbufferUtilベースにしました。

--- src/main/java/com/ttProject/media/mp4/Atom.java
+++ src/main/java/com/ttProject/media/mp4/Atom.java
@@ -3,6 +3,7 @@ package com.ttProject.media.mp4;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 
+import com.ttProject.library.BufferUtil;
 import com.ttProject.nio.channels.IFileReadChannel;
 
 public abstract class Atom {
@@ -22,24 +23,9 @@ public abstract class Atom {
 	}
 	// そのままコピーする動作も必要
 	public void copy(IFileReadChannel ch, WritableByteChannel target) throws Exception {
-		// コピーするデータ量をなんとかしなければいけないと思う。
 		// 開始位置をいれておく
 		ch.position(getPosition());
-		// ここからsize分読み込んで記入していく。
-		ByteBuffer buffer = null;
-		int targetSize = getSize();
-		while(targetSize > 0) {
-			// 結構な速度で動作可能っぽいです。(光回線だからですかね)
-			int size = (167772160 > targetSize) ? targetSize : 167772160;
-			buffer = ByteBuffer.allocate(size);
-			ch.read(buffer);
-			buffer.flip();
-			if(buffer.remaining() == 0) {
-				break;
-			}
-			targetSize -= buffer.remaining();
-			target.write(buffer);
-		}
+		BufferUtil.quickCopy(ch, target, getSize());
 	}
 	public int getSize() {
 		return size;
