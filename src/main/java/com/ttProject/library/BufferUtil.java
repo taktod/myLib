70a56ec0834b9ef0b2019b9f689c3483b339e15d
Toda Takahiko <poepoemix@hotmail.com>
2013-05-04 09:11:44 +0900

    バッファコピー命令を追加

--- src/main/java/com/ttProject/library/BufferUtil.java
+++ src/main/java/com/ttProject/library/BufferUtil.java
@@ -2,6 +2,7 @@ package com.ttProject.library;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 
 import com.ttProject.nio.channels.IFileReadChannel;
 
@@ -54,6 +55,29 @@ public class BufferUtil {
 			}
 			Thread.sleep(10);
 			count ++;
+		}
+	}
+	/**
+	 * 指定したサイズのデータをコピーする。
+	 * @param source
+	 * @param target
+	 * @param size
+	 * @throws Exception
+	 */
+	public static void quickCopy(IFileReadChannel source, WritableByteChannel target, int size) throws Exception {
+		ByteBuffer buffer = null;
+		int targetSize = size;
+		while(targetSize > 0) {
+			int bufSize = (16777216 > targetSize) ? targetSize : 16777216;
+			buffer = ByteBuffer.allocate(bufSize);
+			source.read(buffer);
+			buffer.flip();
+			if(buffer.remaining() == 0) {
+				// ここで抜ける場合は例外にしておいた方が本当はよさそう。(中途で読み込みが完全にできなくなった場合になるため。)
+				break;
+			}
+			targetSize -= buffer.remaining();
+			target.write(buffer);
 		}
 	}
 }
