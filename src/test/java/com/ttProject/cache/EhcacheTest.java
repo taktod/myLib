f6aacf20b4f331fee64b48e1fcc5bee84d7b5440
Toda Takahiko <poepoemix@hotmail.com>
2013-05-06 23:12:44 +0900

    cacheのイベントについて、調査しておいた。 expireのタイミングでイベントがfireされるのでehcacheはいろいろと使えそう。

--- src/test/java/com/ttProject/cache/EhcacheTest.java
+++ src/test/java/com/ttProject/cache/EhcacheTest.java
@@ -1,8 +1,11 @@
 package com.ttProject.cache;
 
 import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
 
 import org.junit.Test;
 
@@ -19,8 +22,8 @@ public class EhcacheTest {
 			1, // あまりにデータ量がおおい場合はdiskcacheにまわす。
 			true, // overFlowしたものはdiskに書き込んでcacheさせる
 			false, // 永続にはしないでexpireが有効になるようにしておく
-			0, // maxLiveは0にしておいて作成してからのexpireはつけない
-			5); // maxIdleは適当な値をつけておいて、アクセスがなかったらexpireするようにしておく
+			5, // maxLiveは0にしておいて作成してからのexpireはつけない
+			0); // maxIdleは適当な値をつけておいて、アクセスがなかったらexpireするようにしておく
 		manager.addCache(cache);
 		return cache;
 	}
@@ -28,6 +31,38 @@ public class EhcacheTest {
 	public void ehcacheTest() {
 		try {
 			Cache cache = getEncacheInstance();
+			cache.getCacheEventNotificationService().registerListener(new CacheEventListener() {
+				@Override
+				public Object clone() throws CloneNotSupportedException {
+					return super.clone();
+				}
+				public void notifyRemoveAll(Ehcache arg0) {
+					System.out.println("remove all");
+				}
+				public void notifyElementUpdated(Ehcache arg0, Element arg1)
+						throws CacheException {
+					System.out.println("notify element update");
+				}
+				public void notifyElementRemoved(Ehcache arg0, Element arg1)
+						throws CacheException {
+					System.out.println("notify element removed");
+				}
+				public void notifyElementPut(Ehcache arg0, Element arg1)
+						throws CacheException {
+					System.out.println("notify element put");
+				}
+				public void notifyElementExpired(Ehcache arg0, Element arg1) {
+					// これでexpireしたというイベントが取得可能なわけか・・・
+					System.out.println(arg1);
+					System.out.println("notify element expired");
+				}
+				public void notifyElementEvicted(Ehcache arg0, Element arg1) {
+					System.out.println("notify element evicted");
+				}
+				public void dispose() {
+					System.out.println("dispose");
+				}
+			});
 			// とりあえずcacheにデータをぶち込んでみようか
 			cache.put(new Element("a", "b"));
 			Thread.sleep(2000);
@@ -37,15 +72,17 @@ public class EhcacheTest {
 			e = cache.get("a");
 			System.out.println(e.getValue());
 			Thread.sleep(2000);
-			e = cache.get("a");
-			System.out.println(e.getValue());
+//			e = cache.get("a");
+//			System.out.println(e.getValue());
+			Thread.sleep(200000);
+			System.out.println("owari");
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	/**
-	 * 大きなデータをぶち込んでみるテストをやってみる。
+	 * バイナリデータ
 	 */
 	@Test
 	public void binaryTest() {
@@ -55,6 +92,18 @@ public class EhcacheTest {
 			cache.put(new Element("binary", "test".getBytes()));
 			Element e = cache.get("binary");
 			System.out.println(HexUtils.toHex((byte[])e.getValue(), true));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * streamの形で徐々に取得するってできないだろうかな・・・
+	 */
+	@Test
+	public void streamTest() {
+		try {
+//			Cache cache = getEncacheInstance();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
