a1ee17ad63cf4103cbe83bac422754c622fc78de
taktod <poepoemix@hotmail.com>
2013-05-25 10:45:46 +0900

    一時ファイル用の動作を作成。

---
+++ myLib.util/src/main/java/com/ttProject/util/TmpFile.java
@@ -0,0 +1,19 @@
+package com.ttProject.util;
+
+import java.io.File;
+
+/**
+ * 一時ファイル作成用のプログラム
+ * @author taktod
+ */
+public class TmpFile extends File {
+	private static final long serialVersionUID = -5496373580008414933L;
+	// 時間が経つと消す？
+	public final long expire = 3600;
+	public TmpFile(String path) {
+		// 一時ファイルを作成します。
+		super(System.getProperty("java.io.tmpdir") + path);
+		getParentFile().mkdirs();
+		deleteOnExit(); // プロセス完了時に消しておく。
+	}
+}
