66ad96ab0885bff9bff95967b14031f1d047b348
taktod <poepoemix@hotmail.com>
2013-06-04 19:56:43 +0900

    日付用の補助関数

---
+++ myLib.util/src/main/java/com/ttProject/util/DateUtil.java
@@ -0,0 +1,36 @@
+package com.ttProject.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * 日付関連の処理補助
+ * @author taktod
+ */
+public class DateUtil {
+	/**
+	 * 日付文字列
+	 * @return
+	 */
+	public static String makeDate() {
+		return make(new SimpleDateFormat("yyyyMMdd"));
+	}
+	/**
+	 * 時刻文字列
+	 * @return
+	 */
+	public static String makeDateTime() {
+		return make(new SimpleDateFormat("yyyyMMdd_HHmmss"));
+	}
+	/**
+	 * 動作補助
+	 * @param dateFormat
+	 * @return
+	 */
+	private static String make(DateFormat dateFormat) {
+		dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
+		return dateFormat.format(new Date());
+	}
+}
