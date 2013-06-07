17950e09b4d3c2c035c10e2884f929c937a87139
taktod <poepoemix@hotmail.com>
2013-06-08 08:24:19 +0900

    ポート番号をpidからつくるjmxのserver動作をつくりました。

--- myLib.jmx/src/main/java/com/ttProject/jmx/JMXServerFactory.java
+++ myLib.jmx/src/main/java/com/ttProject/jmx/JMXServerFactory.java
@@ -1,33 +1,56 @@
 package com.ttProject.jmx;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.rmi.registry.LocateRegistry;
 
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
 
-import org.apache.log4j.Logger;
-
 /**
  * JMX用のリモートサーバーをたてます。
  * @author taktod
  */
 public class JMXServerFactory {
-	/** 動作ロガー */
-	private static final Logger logger = Logger.getLogger(JMXServerFactory.class);
+	private static int port;
+	/**
+	 * 動作ポート番号参照
+	 * @return
+	 */
+	public static int getPort() {
+		return port;
+	}
 	/**
 	 * jmxのリモート用のサーバーをたてておきます
 	 * @param port たてるサーバーのポート番号
 	 */
-	public static void openJMXRemoteServer(int port) {
-		try {
-			LocateRegistry.createRegistry(port);
-			JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + port + "/jmxrmi");
-			JMXConnectorServer server = JMXConnectorServerFactory.newJMXConnectorServer(url, null, JMXFactory.getMBeanServer());
-			server.start();
+	public static void openJMXRemoteServer(int port) throws Exception {
+		JMXServerFactory.port = port;
+		LocateRegistry.createRegistry(port);
+		JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + port + "/jmxrmi");
+		JMXConnectorServer server = JMXConnectorServerFactory.newJMXConnectorServer(url, null, JMXFactory.getMBeanServer());
+		server.start();
+	}
+	/**
+	 * jmxのリモート用のサーバーをたてておきます。
+	 * ポート番号はpid + 1000xとしておきます。
+	 * @throws Exception
+	 */
+	public static void openJMXRemoteServer() throws Exception {
+		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+		int pid = Integer.parseInt(bean.getName().split("@")[0]);
+		if(pid < 1000) {
+			pid += 1000;
 		}
-		catch (Exception e) {
-			logger.error("jmxのリモートサーバーを開くときに失敗しました。", e);
+		for(;pid < 65535;pid += 1000) {
+			try {
+				openJMXRemoteServer(pid);
+				return;
+			}
+			catch (Exception e) {
+			}
 		}
+		throw new Exception("jmx用のポート番号が決定できませんでした");
 	}
 }
