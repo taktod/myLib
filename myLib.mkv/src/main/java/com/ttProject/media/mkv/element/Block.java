d0820122ff0eae363e20ea7cffba03c97bc5b060
taktod <poepoemix@hotmail.com>
2013-06-26 21:11:41 +0900

    lacingで格納されているサイズデータを取得するプログラムをとりあえず、Blockにかいてみた。

--- myLib.mkv/src/main/java/com/ttProject/media/mkv/element/Block.java
+++ myLib.mkv/src/main/java/com/ttProject/media/mkv/element/Block.java
@@ -1,9 +1,12 @@
 package com.ttProject.media.mkv.element;
 
+import java.nio.ByteBuffer;
+
 import com.ttProject.media.mkv.Element;
 import com.ttProject.media.mkv.IElementAnalyzer;
 import com.ttProject.media.mkv.Type;
 import com.ttProject.nio.channels.IFileReadChannel;
+import com.ttProject.util.BufferUtil;
 
 /**
  * 音声と映像の実データを保持しているっぽい
@@ -28,6 +31,76 @@ public class Block extends Element {
 	@Override
 	public void analyze(IFileReadChannel ch, IElementAnalyzer analyzer)
 			throws Exception {
+		// blockの内容を解析します。
+		ch.position((int)getDataPosition());
+		// trackId取得
+		int trackId = (int)getEbmlNumber(ch);
+		ByteBuffer buffer = BufferUtil.safeRead(ch, 3);
+		int timeDiff = buffer.getShort();
+		byte flg = buffer.get();
+		// flgを確認する(とりあえずしない、lacingのみ調べておく。)
+		byte lacingType = (byte)((flg >> 1) & 0x03);
+		switch(lacingType) {
+		case 0:
+			System.out.println("no lacing");
+			break;
+		case 1:
+			{
+				System.out.println("Xiph lacing");
+				int laceCount = BufferUtil.safeRead(ch, 1).get();
+				for(int i = 0;i < laceCount;i ++) {
+					int size = 0;
+					int newDat = 0;
+					while((newDat = (BufferUtil.safeRead(ch, 1).get() & 0xFF)) == 0xFF) {
+						size += 0xFF;
+					}
+					size += newDat;
+					System.out.println(Integer.toHexString(size));
+				}
+			}
+			break;
+		case 2:
+			{
+				System.out.println("fixed-size lacing");
+				int laceCount = BufferUtil.safeRead(ch, 1).get();
+				int totalSize = (int)(getSize() - ch.position() + getDataPosition());
+				int laceSize = totalSize / (laceCount + 1);
+				for(int i = 0;i < laceCount;i ++) {
+					System.out.println(Integer.toHexString(laceSize));
+				}
+			}
+			break;
+		case 3:
+			{
+				System.out.println("ebml lacing");
+				// 次の１バイトがいくつlacingしているか示している。
+				int laceCount = BufferUtil.safeRead(ch, 1).get();
+				System.out.println(laceCount);
+				int laceSize = (int)getEbmlNumber(ch);
+				System.out.println(Integer.toHexString(laceSize));
+				laceCount --;
+				while(laceCount > 0) {
+					int nextLaceDiff = (int)getEbmlNumber(ch);
+					if(nextLaceDiff <= 0x7f) {
+						laceSize += nextLaceDiff - 0x3F;
+					}
+					else if(nextLaceDiff <= 0x3FFF){
+						laceSize += nextLaceDiff - 0x1FFF;
+					}
+					else if(nextLaceDiff <= 0x1FFFFF) {
+						laceSize += nextLaceDiff - 0x0FFFFF;
+					}
+					else if(nextLaceDiff <= 0x0FFFFFFF) {
+						laceSize += nextLaceDiff - 0x07FFFFFF;
+					}
+					System.out.println(Integer.toHexString(laceSize));
+					laceCount --;
+				}
+			}
+			break;
+		}
+		System.out.println(flg);
+		System.out.println(trackId);
 	}
 	@Override
 	public String toString() {
