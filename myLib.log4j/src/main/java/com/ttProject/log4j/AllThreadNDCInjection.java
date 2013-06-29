8a9124226b4fc805a7aed9f4263d2eb3e38e7547
taktod <poepoemix@hotmail.com>
2013-06-29 09:11:51 +0900

    ちょっと修正しておいた。

--- myLib.log4j/src/main/java/com/ttProject/log4j/AllThreadNDCInjection.java
+++ myLib.log4j/src/main/java/com/ttProject/log4j/AllThreadNDCInjection.java
@@ -8,8 +8,11 @@ import java.util.List;
  * @author taktod
  */
 public class AllThreadNDCInjection {
+	/** 登録したいデータ */
 	protected static List<String> data = new ArrayList<String>();
+	/** 登録したいデータ設定 */
 	public static void setup(String ... list) {
+		data.clear();
 		for(String item : list) {
 			data.add(item);
 		}
--- myLib.log4j/src/main/java/com/ttProject/log4j/NDCInjectAppender.java
+++ myLib.log4j/src/main/java/com/ttProject/log4j/NDCInjectAppender.java
@@ -4,10 +4,15 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.NDC;
 import org.apache.log4j.spi.LoggingEvent;
 
+/**
+ * NDCの情報をデータ挿入時に合わせるためのappender
+ * ログの書き込み処理というより、データ書き込み時Hookとして利用しています。
+ * できたら、ロガーの順位として上位にいれておくと吉です。
+ * @author taktod
+ */
 public class NDCInjectAppender extends AppenderSkeleton {
 	@Override
 	public void close() {
-
 	}
 	@Override
 	public boolean requiresLayout() {
@@ -15,6 +20,7 @@ public class NDCInjectAppender extends AppenderSkeleton {
 	}
 	@Override
 	protected void append(LoggingEvent event) {
+		// 書き込み時に登録したいデータとNDCに登録されているデータの数を比べてずれている場合は、初期化しなおす。
 		if(NDC.getDepth() != AllThreadNDCInjection.data.size()) {
 			NDC.clear();
 			for(String item : AllThreadNDCInjection.data) {
