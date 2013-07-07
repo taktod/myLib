7023703772e07a48cc542fa011b2719dad9086d1
taktod <poepoemix@hotmail.com>
2013-07-07 20:50:04 +0900

    rtmpで任意のデータをpublishする動作をテストとして書いてみた。 標準入力のデータも遅れるはずなんだけどねぇ。

---
+++ myLib.flazr/src/test/java/com/ttProject/flazr/test/FlvTestReader.java
@@ -0,0 +1,102 @@
+package com.ttProject.flazr.test;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.flazr.io.flv.FlvAtom;
+import com.flazr.rtmp.RtmpMessage;
+import com.flazr.rtmp.RtmpReader;
+import com.flazr.rtmp.message.Metadata;
+import com.flazr.rtmp.message.MetadataAmf0;
+import com.ttProject.flazr.TagManager;
+import com.ttProject.media.flv.FlvHeader;
+import com.ttProject.media.flv.ITagAnalyzer;
+import com.ttProject.media.flv.Tag;
+import com.ttProject.media.flv.TagAnalyzer;
+import com.ttProject.nio.channels.FileReadChannel;
+import com.ttProject.nio.channels.IFileReadChannel;
+
+public class FlvTestReader implements RtmpReader {
+	private final LinkedBlockingQueue<FlvAtom> dataQueue = new LinkedBlockingQueue<FlvAtom>();
+	private Metadata metadata;
+	private int aggregateDuration;
+	private final Thread readThread;
+	public FlvTestReader() {
+		readThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// flvデータの読み込みを実施してデータを送り込む
+				System.out.println("読み込み処理開始");
+				try {
+					IFileReadChannel channel = FileReadChannel.openFileReadChannel("http://49.212.39.17/mario.flv");
+					FlvHeader header = new FlvHeader();
+					header.analyze(channel);
+					ITagAnalyzer analyzer = new TagAnalyzer();
+					Tag tag = null;
+					TagManager manager = new TagManager();
+					while((tag = analyzer.analyze(channel)) != null) {
+						FlvAtom atom = manager.getAtom(tag);
+						dataQueue.add(atom);
+						System.out.println(tag);
+					}
+				}
+				catch (Exception e) {
+				}
+			}
+		});
+		readThread.start();
+		metadata = new MetadataAmf0("onMetaData");
+	}
+	@Override
+	public void close() {
+		readThread.interrupt();
+	}
+
+	@Override
+	public Metadata getMetadata() {
+		return metadata;
+	}
+
+	@Override
+	public RtmpMessage[] getStartMessages() {
+		return new RtmpMessage[]{metadata};
+	}
+
+	@Override
+	@Deprecated
+	public long getTimePosition() {
+		// live動作なのでシーク禁止
+		throw new RuntimeException("getTimePositionが呼ばれました。");
+	}
+
+	@Override
+	public boolean hasNext() {
+		return true;
+	}
+
+	@Override
+	public RtmpMessage next() {
+		if(aggregateDuration <= 0) {
+			try {
+				return dataQueue.take();
+			}
+			catch (Exception e) {
+				throw new RuntimeException("takeに失敗したよ");
+			}
+		}
+		else {
+			throw new RuntimeException("aggregateDurationが0よりおおきいです。");
+		}
+	}
+
+	@Override
+	@Deprecated
+	public long seek(long time) {
+		throw new RuntimeException("seekが呼ばれました。");
+	}
+
+	@Override
+	public void setAggregateDuration(int targetDuration) {
+		aggregateDuration = targetDuration;
+
+	}
+}
---
+++ myLib.flazr/src/test/java/com/ttProject/flazr/test/PublishTest.java
@@ -0,0 +1,68 @@
+package com.ttProject.flazr.test;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+
+import com.flazr.rtmp.RtmpDecoder;
+import com.flazr.rtmp.RtmpEncoder;
+import com.flazr.rtmp.client.ClientHandler;
+import com.flazr.rtmp.client.ClientHandshakeHandler;
+import com.flazr.rtmp.client.ClientOptions;
+
+public class PublishTest {
+	public static void main(String[] args) {
+		final ClientOptions options = new ClientOptions();
+		if(!options.parseCli(args)) {
+			return;
+		}
+		if(options.getLoad() == 1 && options.getClientOptionsList() == null) {
+			// 単一動作のみ
+			// rtmpReaderを適当に作る必要あり。
+			options.setFileToPublish(null);
+			options.setReaderToPublish(new FlvTestReader());
+			connect(options);
+			return;
+		}
+		else {
+			throw new RuntimeException("マルチ起動は許可しておりません。");
+		}
+	}
+	private static void connect(final ClientOptions options) {
+		final ClientBootstrap bootstrap = getBootstrap(Executors.newCachedThreadPool(), options);
+		final ChannelFuture future = bootstrap.connect(new InetSocketAddress(options.getHost(), options.getPort()));
+		future.awaitUninterruptibly();
+		if(!future.isSuccess()) {
+			
+		}
+		future.getChannel().getCloseFuture().awaitUninterruptibly();
+		bootstrap.getFactory().releaseExternalResources();
+	}
+	private static ClientBootstrap getBootstrap(final Executor executor, final ClientOptions options) {
+		final ChannelFactory factory = new NioClientSocketChannelFactory(executor, executor);
+		final ClientBootstrap bootstrap = new ClientBootstrap(factory);
+		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+			@Override
+			public ChannelPipeline getPipeline() throws Exception {
+				ChannelPipeline pipeline = Channels.pipeline();
+				pipeline.addLast("handshaker", new ClientHandshakeHandler(options));
+				pipeline.addLast("decoder", new RtmpDecoder()); // amf3を使う理由もないので、通常のrtmpDecoderでOK
+				pipeline.addLast("encoder", new RtmpEncoder());
+				pipeline.addLast("handler", new ClientHandler(options));
+				return pipeline;
+			}
+		});
+		
+		bootstrap.setOption("tcpNoDelay", true);
+		bootstrap.setOption("keepAlive", true);
+		return bootstrap;
+	}
+}
