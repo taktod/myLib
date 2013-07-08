6ec88dbe8f9eb6a988ed96c31dc3e1fab88f8a1d
taktod <poepoemix@hotmail.com>
2013-07-08 20:59:00 +0900

    解析テストをまとめてみた。

---
+++ myLib.media.flv/src/test/java/com/ttProject/flv/test/FileAnalyzeTest.java
@@ -0,0 +1,58 @@
+package com.ttProject.flv.test;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+import com.ttProject.media.flv.FlvHeader;
+import com.ttProject.media.flv.FlvManager;
+import com.ttProject.media.flv.ITagAnalyzer;
+import com.ttProject.media.flv.Tag;
+import com.ttProject.media.flv.TagAnalyzer;
+import com.ttProject.nio.channels.FileReadChannel;
+import com.ttProject.nio.channels.IFileReadChannel;
+import com.ttProject.util.BufferUtil;
+
+/**
+ * flvの解析テスト
+ * @author taktod
+ */
+public class FileAnalyzeTest {
+	/**
+	 * ファイル全体を解析するテスト
+	 */
+	@Test
+	public void fixedFileTest() throws Exception {
+		IFileReadChannel source = FileReadChannel.openFileReadChannel("http://49.212.39.17/mario.flv");
+		FlvHeader flvheader = new FlvHeader();
+		flvheader.analyze(source);
+		System.out.println(flvheader);
+		ITagAnalyzer analyzer = new TagAnalyzer();
+		// sourceをそのまま解析する。
+		Tag tag = null;
+		while((tag = analyzer.analyze(source)) != null) {
+			System.out.println(tag);
+		}
+		source.close();
+	}
+	/**
+	 * サイズがわかっていないデータを順に受け取るときにflvを解析する動作テスト
+	 */
+	@Test
+	public void appendingBufferTest() throws Exception {
+		IFileReadChannel source = FileReadChannel.openFileReadChannel("http://49.212.39.17/mario.flv");
+		FlvHeader flvheader = new FlvHeader();
+		flvheader.analyze(source);
+		System.out.println(flvheader);
+		ByteBuffer buffer = BufferUtil.safeRead(source, 2560);
+		FlvManager manager = new FlvManager();
+		for(Tag tag : manager.getUnits(buffer)) {
+			System.out.println(tag);
+		}
+		buffer = BufferUtil.safeRead(source, 2560);
+		for(Tag tag : manager.getUnits(buffer)) {
+			System.out.println(tag);
+		}
+		source.close();
+	}
+}
--- myLib.media.mp3/src/test/java/com/ttProject/media/mp3/test/FileAnalyzeTest.java
+++ myLib.media.mp3/src/test/java/com/ttProject/media/mp3/test/FileAnalyzeTest.java
@@ -21,57 +21,47 @@ public class FileAnalyzeTest {
 	 * ファイルサイズが固定されている状態での動作テスト
 	 */
 	@Test
-	public void fixedFileTest() {
-		try {
-			IReadChannel channel = FileReadChannel.openFileReadChannel(
-					Thread.currentThread().getContextClassLoader().getResource("sample.mp3")
-			);
-			IFrameAnalyzer analyzer = new FrameAnalyzer();
-			// channelをそのまま解析します。
-			Frame frame = null;
-			while((frame = analyzer.analyze(channel)) != null) {
-				System.out.println(frame);
-			}
-			// 最後までいったらおわり
-			channel.close();
+	public void fixedFileTest() throws Exception {
+		IReadChannel source = FileReadChannel.openFileReadChannel(
+				Thread.currentThread().getContextClassLoader().getResource("sample.mp3")
+		);
+		IFrameAnalyzer analyzer = new FrameAnalyzer();
+		// sourceをそのまま解析します。
+		Frame frame = null;
+		while((frame = analyzer.analyze(source)) != null) {
+			System.out.println(frame);
 		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		// 最後までいったらおわり
+		source.close();
 	}
 	/**
 	 * ファイルサイズはわからないがbyteBufferデータが順番に追加される状態での動作テスト
 	 * stdinみたいにサイズがわかっていないデータはこちら側
 	 */
 	@Test
-	public void appendingBufferTest() {
-		try {
-			IReadChannel source = FileReadChannel.openFileReadChannel(
-					Thread.currentThread().getContextClassLoader().getResource("sample.mp3")
-			);
-			// 適当な量ずつデータを取り出します。
-			ByteBuffer buffer = BufferUtil.safeRead(source, 2560);
-			Mp3Manager manager = new Mp3Manager();
-			// 解析にまわす。
-			for(Frame tag : manager.getUnits(buffer)) {
-				System.out.println(tag);
-			}
-			// 続きのデータを読み込む
-			buffer = BufferUtil.safeRead(source, 256);
-			// 解析にまわす
-			for(Frame tag : manager.getUnits(buffer)) {
-				System.out.println(tag);
-			}
-			// さらにつづける
-			buffer = BufferUtil.safeRead(source, 256);
-			for(Frame tag : manager.getUnits(buffer)) {
-				System.out.println(tag);
-			}
-			// 飽きたらやめる
-			source.close();
+	public void appendingBufferTest() throws Exception {
+		IReadChannel source = FileReadChannel.openFileReadChannel(
+				Thread.currentThread().getContextClassLoader().getResource("sample.mp3")
+		);
+		// 適当な量ずつデータを取り出します。
+		ByteBuffer buffer = BufferUtil.safeRead(source, 2560);
+		Mp3Manager manager = new Mp3Manager();
+		// 解析にまわす。
+		for(Frame tag : manager.getUnits(buffer)) {
+			System.out.println(tag);
 		}
-		catch (Exception e) {
-			e.printStackTrace();
+		// 続きのデータを読み込む
+		buffer = BufferUtil.safeRead(source, 256);
+		// 解析にまわす
+		for(Frame tag : manager.getUnits(buffer)) {
+			System.out.println(tag);
 		}
+		// さらにつづける
+		buffer = BufferUtil.safeRead(source, 256);
+		for(Frame tag : manager.getUnits(buffer)) {
+			System.out.println(tag);
+		}
+		// 飽きたらやめる
+		source.close();
 	}
 }
