8dba84b10dd86a5eec4a64473a64c4ee19381717
taktod <poepoemix@hotmail.com>
2013-07-08 20:16:49 +0900

    もともと各unitのstaticとして設定していた、データの読み込み部を別のクラスにわけたいと思った。

---
+++ myLib.media/src/main/java/com/ttProject/media/Manager.java
@@ -0,0 +1,42 @@
+package com.ttProject.media;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import com.ttProject.nio.channels.IReadChannel;
+
+/**
+ * メディアデータを扱うマネージャー
+ * @author taktod
+ */
+public abstract class Manager<T> {
+	private ByteBuffer buffer = null;
+	/**
+	 * 追加bufferを足して、データを応答します。
+	 * 応答データは読み込みモードとして返ってきます。
+	 * @param data 読み込みモードの適当なデータ
+	 * @return 読み込みモードの合成後のデータ(データがnullのこともあります。)
+	 */
+	protected ByteBuffer appendBuffer(ByteBuffer data) {
+		if(data == null) {
+			return buffer;
+		}
+		if(buffer != null) {
+			int length = buffer.remaining() + data.remaining();
+			ByteBuffer newBuffer = ByteBuffer.allocate(length);
+			newBuffer.put(buffer);
+			buffer = newBuffer;
+		}
+		else {
+			int length = data.remaining();
+			buffer = ByteBuffer.allocate(length);
+		}
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
+	}
+	// データをいれると、取得可能になったデータがでてくる。
+	public abstract List<T> getUnits(ByteBuffer data);
+	// IReadChannelをいれると、１つ分のデータが取得できる。
+	public abstract T getUnit(IReadChannel source) throws Exception;
+}
