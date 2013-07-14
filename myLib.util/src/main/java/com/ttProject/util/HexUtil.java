342c58c26f2a34426c8eb552f9f69e6e8bfebe41
taktod <poepoemix@hotmail.com>
2013-07-14 15:12:38 +0900

    HexUtilsをHexUtilに変更した。たぶん見えていないエラーいっぱいあると思われ。

--- myLib.util/src/main/java/com/ttProject/util/HexUtil.java
+++ myLib.util/src/main/java/com/ttProject/util/HexUtil.java
@@ -8,7 +8,7 @@ import java.nio.ByteBuffer;
  * 元ネタはflazr
  * @author taktod
  */
-public class HexUtils {
+public class HexUtil {
 	private static final char[] HEX_DIGITS = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F'
