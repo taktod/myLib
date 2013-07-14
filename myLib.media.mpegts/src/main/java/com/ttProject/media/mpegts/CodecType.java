0744d65a388841d3ee4d9aa71def87cf2e948eb6
taktod <poepoemix@hotmail.com>
2013-07-14 09:09:47 +0900

    判定可能なコーデックタイプを記載しといた。

--- myLib.media.mpegts/src/main/java/com/ttProject/media/mpegts/CodecType.java
+++ myLib.media.mpegts/src/main/java/com/ttProject/media/mpegts/CodecType.java
@@ -1,7 +1,25 @@
 package com.ttProject.media.mpegts;
 
+/**
+ * mpegtsの内部にあるコーデック情報
+ * @author taktod
+ */
 public enum CodecType {
-	H264(0x1B);
+	VIDEO_MPEG1(0x01),
+	VIDEO_MPEG2(0x02),
+	AUDIO_MPEG1(0x03), // mp3とか
+	AUDIO_MPEG2(0x04),
+	PRIVATE_SECTION(0x05),
+	PRIVATE_DATA(0x06),
+	AUDIO_AAC(0x0F),
+	VIDEO_MPEG4(0x10),
+	AUDIO_LATM_AAC(0x11),
+	SYSTEMS_MPEG4_PES(0x12),
+	SYSTEMS_MPEG4_SECTIONS(0x13),
+	VIDEO_H264(0x1B),
+	AUDIO_AC3(0x81),
+	AUDIO_DTS(0x8A),
+	SUBTITLE_DVB(0x100);
 	private final int value;
 	private CodecType(int value) {
 		this.value = value;
