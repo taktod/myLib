070d291eafd28967be8b94f133b904a0441ab348
taktod <poepoemix@hotmail.com>
2013-07-20 09:37:03 +0900

    crc32のクラスのリネームとテストコードを書いてみた。

---
+++ myLib.media.mpegts/src/test/java/com/ttProject/media/mpegts/test/Crc32Test.java
@@ -0,0 +1,31 @@
+package com.ttProject.media.mpegts.test;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ttProject.media.mpegts.Crc32;
+import com.ttProject.util.HexUtil;
+
+/**
+ * crc32の動作確認
+ * @author taktod
+ */
+public class Crc32Test {
+	/**
+	 * 計算があうか確認
+	 * @throws Exception
+	 */
+	@Test
+	public void test() throws Exception {
+		Crc32 crc32 = new Crc32();
+		// Patのサンプル474000100000B00D0001C100000001F0002AB104B2
+		// 000B00D0001C100000001F000の部分が計算して2AB104B2になればよい
+		ByteBuffer buffer = HexUtil.makeBuffer("00B00D0001C100000001F000");
+		while(buffer.remaining() != 0) {
+			crc32.update(buffer.get());
+		}
+		Assert.assertEquals("crc32の値確認", 0x2AB104B2, crc32.getValue());
+	}
+}
