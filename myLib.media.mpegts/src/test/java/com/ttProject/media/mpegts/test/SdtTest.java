d11c7338b432cc101100d3475ea395cadfaac7c8
taktod <poepoemix@hotmail.com>
2013-07-20 09:38:01 +0900

    stdのテストケースを作成中

---
+++ myLib.media.mpegts/src/test/java/com/ttProject/media/mpegts/test/SdtTest.java
@@ -0,0 +1,26 @@
+package com.ttProject.media.mpegts.test;
+
+import org.junit.Test;
+
+import com.ttProject.media.mpegts.packet.Sdt;
+import com.ttProject.util.HexUtil;
+
+/**
+ * sdtを生成するときの動作テスト
+ * @author taktod
+ */
+public class SdtTest {
+	/**
+	 * sdtのデータ確認動作テスト
+	 * @throws Exception
+	 */
+	@Test
+	public void check() throws Exception {
+		Sdt sdt = new Sdt(HexUtil.makeBuffer("474011100042F0240001C100000001FF0001FC8013481101054C696261760953657276696365303168C5DB49"));
+		System.out.println(sdt);
+	}
+	@Test
+	public void test() throws Exception {
+		Sdt sdt = new Sdt();
+	}
+}
