fd26df3378d37079c65177cb1339f14069107cd0
taktod <poepoemix@hotmail.com>
2013-08-08 22:25:46 +0900

    中途でへんな風にぬける懸念があったので、修正した。

--- myLib.media.flv/src/main/java/com/ttProject/media/flv/TagAnalyzer.java
+++ myLib.media.flv/src/main/java/com/ttProject/media/flv/TagAnalyzer.java
@@ -1,19 +1,23 @@
 package com.ttProject.media.flv;
 
-import com.ttProject.media.flv.tag.MetaTag;
+import com.ttProject.media.flv.tag.AudioTag;
+import com.ttProject.media.flv.tag.VideoTag;
 import com.ttProject.nio.channels.IReadChannel;
 
 public class TagAnalyzer implements ITagAnalyzer {
 	private final FlvManager manager = new FlvManager();
 	@Override
 	public Tag analyze(IReadChannel ch) throws Exception {
-		Tag tag;
+		Tag tag = null;
 		do {
+			if(tag != null) {
+				ch.position(tag.getPosition() + tag.getSize());
+			}
 			tag = manager.getUnit(ch);
 			if(tag == null) {
 				return null;
 			}
-		} while(!(tag instanceof MetaTag) && tag.getSize() <= 15); // メディアデータなのに、内容がない場合は合っても仕方ないので捨てます。
+		} while((tag instanceof VideoTag || tag instanceof AudioTag) && tag.getSize() <= 15); // メディアデータなのに、内容がない場合は合っても仕方ないので捨てます。
 		// tagデータの実データ部のみ、読み込みさせる。(Tag.getTagを実行すると、fileのpointerが先頭部分だけすすんでいるため。)
 		tag.analyze(ch, false);
 		ch.position(tag.getPosition() + tag.getSize());
