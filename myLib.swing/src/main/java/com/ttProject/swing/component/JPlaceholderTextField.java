b239d068cb80fb0946914a3aa710f6f21148dcf8
taktod <poepoemix@hotmail.com>
2013-08-20 21:31:24 +0900

    swingの動作補助のライブラリがほしくなったので、さくっとplaceholder付きのJTextFieldつくってみた。 まぁ、コピーだけど。

---
+++ myLib.swing/src/main/java/com/ttProject/swing/component/JPlaceholderTextField.java
@@ -0,0 +1,52 @@
+package com.ttProject.swing.component;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JTextField;
+import javax.swing.text.Document;
+
+/**
+ * placeHolder付きのtextField
+ * @see http://stackoverflow.com/questions/16213836/java-swing-jtextfield-set-placeholder
+ * @author taktod
+ */
+public class JPlaceholderTextField extends JTextField {
+	private static final long serialVersionUID = 8838799910774222615L;
+	private String placeholder = null;
+	public JPlaceholderTextField() {
+	}
+	public JPlaceholderTextField(
+			final Document pDoc,
+			final String pText,
+			final int pColumns) {
+		super(pDoc, pText, pColumns);
+	}
+	public JPlaceholderTextField(final String pText) {
+		super(pText);
+	}
+	public JPlaceholderTextField(final int pColumns) {
+		super(pColumns);
+	}
+	public JPlaceholderTextField(final String pText, final int pColumns) {
+		super(pText, pColumns);
+	}
+	public String getPlaceholder() {
+		return placeholder;
+	}
+	public void setPlaceholder(String placeholder) {
+		this.placeholder = placeholder;
+	}
+	@Override
+	protected void paintComponent(Graphics pG) {
+		super.paintComponent(pG);
+		if(placeholder.length() == 0 || getText().length() > 0) {
+			return;
+		}
+		final Graphics2D g = (Graphics2D)pG;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(getDisabledTextColor());
+		g.drawString(placeholder, getInsets().left, pG.getFontMetrics().getMaxAscent() + getInsets().top);
+	}
+}
