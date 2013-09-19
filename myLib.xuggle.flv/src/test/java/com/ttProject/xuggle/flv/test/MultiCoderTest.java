4c8f5f1f43cf75e5f8f4f352dbce007a8491cd1b
taktod <poepoemix@hotmail.com>
2013-09-19 20:00:33 +0900

    h263とavcの並行変換テストも書いてみたけど、こっちは、いれているフレームが時間ベースでいれているので、どっちがいいか、いまいちわからん。

--- myLib.xuggle.flv/src/test/java/com/ttProject/xuggle/flv/test/MultiCoderTest.java
+++ myLib.xuggle.flv/src/test/java/com/ttProject/xuggle/flv/test/MultiCoderTest.java
@@ -1,5 +1,9 @@
 package com.ttProject.xuggle.flv.test;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -13,13 +17,21 @@ import org.junit.Test;
 import com.ttProject.media.flv.FlvHeader;
 import com.ttProject.media.flv.Tag;
 import com.ttProject.media.raw.AudioData;
+import com.ttProject.media.raw.VideoData;
+import com.ttProject.util.DateUtil;
 import com.ttProject.xuggle.flv.FlvDepacketizer;
 import com.ttProject.xuggle.raw.AudioConverter;
 import com.xuggle.xuggler.IAudioSamples;
 import com.xuggle.xuggler.ICodec;
 import com.xuggle.xuggler.IPacket;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IRational;
 import com.xuggle.xuggler.IStreamCoder;
 import com.xuggle.xuggler.IStreamCoder.Direction;
+import com.xuggle.xuggler.IStreamCoder.Flags;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.video.ConverterFactory;
+import com.xuggle.xuggler.video.IConverter;
 
 /**
  * 複数のxuggleを同時に走らせるとどうなるかというテスト
@@ -84,7 +96,7 @@ public class MultiCoderTest {
 	 * この動作からすると、IAudioSamplesの使いまわしは可能っぽいですね。
 	 * @throws Exception
 	 */
-	@Test
+//	@Test
 	public void aacmp3Test() throws Exception {
 		audioCounter = 0;
 		FileChannel mp3stereo =null;
@@ -186,7 +198,7 @@ public class MultiCoderTest {
 	 * 速度かわらず。
 	 */
 	boolean workingFlg = true;
-	@Test
+//	@Test
 	public void aacmp3MultiThreadTest() {
 		audioCounter = 0;
 		workingFlg = true;
@@ -341,7 +353,7 @@ public class MultiCoderTest {
 	 * 早くなりました。
 	 * ソースを別のものとしてしまえばどうということない・・・ということでしょうかね。
 	 */
-	@Test
+//	@Test
 	public void aacmp3MultiThreadTest2() {
 		audioCounter = 0;
 		workingFlg = true;
@@ -485,7 +497,7 @@ public class MultiCoderTest {
 	 * 変換そのものを並列タスクで動作させること自体は問題ないっぽいですね。
 	 * これでthreadがうまく使えそう・・・
 	 */
-	@Test
+//	@Test
 	public void aacmp3MultiThreadTest3() {
 		audioCounter = 0;
 		workingFlg = true;
@@ -629,6 +641,516 @@ public class MultiCoderTest {
 				catch (Exception e) {
 				}
 				aacstereo = null;
+			}
+		}
+	}
+	/**
+	 * 表示画像をつくってみる。
+	 * @return
+	 */
+	private BufferedImage image() {
+		BufferedImage base = new BufferedImage(320, 240, BufferedImage.TYPE_3BYTE_BGR);
+		String message = DateUtil.makeDateTime();
+		Graphics g = base.getGraphics();
+		g.setColor(Color.white);
+		g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 24));
+		g.drawString(message, 100, 100);
+		g.dispose();
+		return base;
+	}
+	/**
+	 * flvとavcの映像を作る動作
+	 */
+	@Test
+	public void h263avcTest() {
+		FileChannel h263 = null;
+		FileChannel avc = null;
+		IStreamCoder h263Encoder = null;
+		IStreamCoder avcEncoder = null;
+		IConverter converter = ConverterFactory.createConverter(new BufferedImage(320, 240, BufferedImage.TYPE_3BYTE_BGR), IPixelFormat.Type.YUV420P);
+		try {
+			h263 = new FileOutputStream("h263_a.flv").getChannel();
+			avc = new FileOutputStream("avc_a.flv").getChannel();
+			FlvHeader flvHeader = new FlvHeader();
+			flvHeader.setVideoFlg(true);
+			flvHeader.setAudioFlg(false);
+			h263.write(flvHeader.getBuffer());
+			avc.write(flvHeader.getBuffer());
+			IStreamCoder encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_FLV1);
+			IRational frameRate = IRational.make(15, 1); // 15fps
+			encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+			encoder.setBitRate(250000); // 250kbps
+			encoder.setBitRateTolerance(9000);
+			encoder.setPixelType(IPixelFormat.Type.YUV420P);
+			encoder.setWidth(320);
+			encoder.setHeight(240);
+			encoder.setGlobalQuality(10);
+			encoder.setFrameRate(frameRate);
+			encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+			if(encoder.open(null, null) < 0) {
+				throw new RuntimeException("エンコーダーが開けませんでした。");
+			}
+			h263Encoder = encoder;
+			encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_H264);
+			frameRate = IRational.make(15, 1); // 15fps
+			encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+			
+			encoder.setBitRate(250000); // 250kbps
+			encoder.setBitRateTolerance(9000);
+			encoder.setPixelType(IPixelFormat.Type.YUV420P);
+			encoder.setWidth(320);
+			encoder.setHeight(240);
+			encoder.setGlobalQuality(10);
+			encoder.setFrameRate(frameRate);
+			encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+			encoder.setProperty("level", "30");
+			encoder.setProperty("coder", "0");
+			encoder.setProperty("qmin", "10");
+			encoder.setProperty("bf", "0");
+			encoder.setProperty("wprefp", "0");
+			encoder.setProperty("cmp", "+chroma");
+			encoder.setProperty("partitions", "-parti8x8+parti4x4+partp8x8+partp4x4-partb8x8");
+			encoder.setProperty("me_method", "hex");
+			encoder.setProperty("subq", "5");
+			encoder.setProperty("me_range", "16");
+			encoder.setProperty("keyint_min", "25");
+			encoder.setProperty("sc_threshold", "40");
+			encoder.setProperty("i_qfactor", "0.71");
+			encoder.setProperty("b_strategy", "0");
+			encoder.setProperty("qcomp", "0.6");
+			encoder.setProperty("qmax", "30");
+			encoder.setProperty("qdiff", "4");
+			encoder.setProperty("directpred", "0");
+			encoder.setProperty("cqp", "0");
+			encoder.setFlag(Flags.FLAG_LOOP_FILTER, true);
+			encoder.setFlag(Flags.FLAG_CLOSED_GOP, true);
+			if(encoder.open(null, null) < 0) {
+				throw new RuntimeException("エンコーダーが開けませんでした。");
+			}
+			avcEncoder = encoder;
+			// 画像データを変換可能なpixelデータに変換する。
+			IPacket packet = IPacket.make();
+			int index = 0;
+			long startTime = -1;
+			FlvDepacketizer h263Depacketizer = new FlvDepacketizer();
+			FlvDepacketizer avcDepacketizer = new FlvDepacketizer();
+			while(true) {
+				index ++;
+				long now = System.currentTimeMillis();
+				if(startTime == -1) {
+					startTime = now;
+				}
+				if(now - startTime > 3000) {
+					break;
+				}
+				BufferedImage image = image();
+				IVideoPicture picture = converter.toPicture(image, 1000 * (now - startTime));
+				if(h263Encoder.encodeVideo(packet, picture, 0) < 0) {
+					throw new Exception("変換失敗");
+				}
+				if(packet.isComplete()) {
+					for(Tag tag : h263Depacketizer.getTag(h263Encoder, packet)) {
+						h263.write(tag.getBuffer());
+					}
+				}
+				if(avcEncoder.encodeVideo(packet, picture, 0) < 0) {
+					throw new Exception("変換失敗");
+				}
+				if(packet.isComplete()) {
+					for(Tag tag : avcDepacketizer.getTag(avcEncoder, packet)) {
+						avc.write(tag.getBuffer());
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(h263Encoder != null) {
+				h263Encoder.close();
+				h263Encoder = null;
+			}
+			if(avcEncoder != null) {
+				avcEncoder.close();
+				avcEncoder = null;
+			}
+			if(h263 != null) {
+				try {
+					h263.close();
+				}
+				catch (Exception e) {
+				}
+				h263 = null;
+			}
+			if(avc != null) {
+				try {
+					avc.close();
+				}
+				catch (Exception e) {
+				}
+				avc = null;
+			}
+		}
+	}
+//	@Test
+	public void h263avcMultiThreadTest() {
+		workingFlg = true;
+		FileChannel h263 = null;
+		FileChannel avc = null;
+		IConverter converter = ConverterFactory.createConverter(new BufferedImage(320, 240, BufferedImage.TYPE_3BYTE_BGR), IPixelFormat.Type.YUV420P);
+		try {
+			final LinkedBlockingQueue<IVideoPicture> h263Pictures = new LinkedBlockingQueue<IVideoPicture>();
+			final LinkedBlockingQueue<IVideoPicture> avcPictures = new LinkedBlockingQueue<IVideoPicture>();
+			h263 = new FileOutputStream("h263_b.flv").getChannel();
+			avc = new FileOutputStream("avc_b.flv").getChannel();
+			FlvHeader flvHeader = new FlvHeader();
+			flvHeader.setVideoFlg(true);
+			flvHeader.setAudioFlg(false);
+			h263.write(flvHeader.getBuffer());
+			avc.write(flvHeader.getBuffer());
+			
+			final FileChannel h263Channel = h263;
+			final FileChannel avcChannel = avc;
+			Thread h263Thread = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					IStreamCoder encoder = null;
+					try {
+						encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_FLV1);
+						IRational frameRate = IRational.make(15, 1); // 15fps
+						encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+						encoder.setBitRate(250000); // 250kbps
+						encoder.setBitRateTolerance(9000);
+						encoder.setPixelType(IPixelFormat.Type.YUV420P);
+						encoder.setWidth(320);
+						encoder.setHeight(240);
+						encoder.setGlobalQuality(10);
+						encoder.setFrameRate(frameRate);
+						encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+						if(encoder.open(null, null) < 0) {
+							throw new RuntimeException("エンコーダーが開けませんでした。");
+						}
+						IPacket packet = IPacket.make();
+						FlvDepacketizer depacketizer = new FlvDepacketizer();
+						while(workingFlg || h263Pictures.size() > 0) {
+							System.out.println("h263コンバート");
+							if(encoder.encodeVideo(packet, h263Pictures.take(), 0) < 0) {
+								throw new Exception("エンコード失敗");
+							}
+							if(packet.isComplete()) {
+								for(Tag tag : depacketizer.getTag(encoder, packet)) {
+									h263Channel.write(tag.getBuffer());
+								}
+							}
+						}
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					finally {
+						if(encoder != null) {
+							encoder.close();
+							encoder = null;
+						}
+					}
+				}
+			});
+			h263Thread.setName("h263ConvertThread");
+			h263Thread.start();
+			Thread avcThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					IStreamCoder encoder = null;
+					try {
+						encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_H264);
+						IRational frameRate = IRational.make(15, 1); // 15fps
+						encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+						
+						encoder.setBitRate(250000); // 250kbps
+						encoder.setBitRateTolerance(9000);
+						encoder.setPixelType(IPixelFormat.Type.YUV420P);
+						encoder.setWidth(320);
+						encoder.setHeight(240);
+						encoder.setGlobalQuality(10);
+						encoder.setFrameRate(frameRate);
+						encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+						encoder.setProperty("level", "30");
+						encoder.setProperty("coder", "0");
+						encoder.setProperty("qmin", "10");
+						encoder.setProperty("bf", "0");
+						encoder.setProperty("wprefp", "0");
+						encoder.setProperty("cmp", "+chroma");
+						encoder.setProperty("partitions", "-parti8x8+parti4x4+partp8x8+partp4x4-partb8x8");
+						encoder.setProperty("me_method", "hex");
+						encoder.setProperty("subq", "5");
+						encoder.setProperty("me_range", "16");
+						encoder.setProperty("keyint_min", "25");
+						encoder.setProperty("sc_threshold", "40");
+						encoder.setProperty("i_qfactor", "0.71");
+						encoder.setProperty("b_strategy", "0");
+						encoder.setProperty("qcomp", "0.6");
+						encoder.setProperty("qmax", "30");
+						encoder.setProperty("qdiff", "4");
+						encoder.setProperty("directpred", "0");
+						encoder.setProperty("cqp", "0");
+						encoder.setFlag(Flags.FLAG_LOOP_FILTER, true);
+						encoder.setFlag(Flags.FLAG_CLOSED_GOP, true);
+						if(encoder.open(null, null) < 0) {
+							throw new RuntimeException("エンコーダーが開けませんでした。");
+						}
+						IPacket packet = IPacket.make();
+						FlvDepacketizer depacketizer = new FlvDepacketizer();
+						while(workingFlg || avcPictures.size() > 0) {
+							System.out.println("avcコンバート");
+							if(encoder.encodeVideo(packet, avcPictures.take(), 0) < 0) {
+								throw new Exception("エンコード失敗");
+							}
+							if(packet.isComplete()) {
+								System.out.println("packet complete");
+								for(Tag tag : depacketizer.getTag(encoder, packet)) {
+									avcChannel.write(tag.getBuffer());
+								}
+							}
+						}
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					finally {
+						if(encoder != null) {
+							encoder.close();
+							encoder = null;
+						}
+					}
+				}
+			});
+			avcThread.setName("avcConvertThread");
+			avcThread.start();
+			long startTime = -1;
+			while(true) {
+				long now = System.currentTimeMillis();
+				if(startTime == -1) {
+					startTime = now;
+				}
+				if(now - startTime > 5000) {
+					workingFlg = false;
+					break;
+				}
+				Thread.sleep(100);
+				BufferedImage image = image();
+				IVideoPicture picture = converter.toPicture(image, 1000 * (now - startTime));
+				avcPictures.add(picture);
+				picture = converter.toPicture(image, 1000 * (now - startTime));
+				h263Pictures.add(picture);
+			}
+			System.out.println("処理おわり、thread完了待ち");
+			h263Thread.interrupt();
+			avcThread.interrupt();
+//			h263Thread.join();
+//			avcThread.join();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(h263 != null) {
+				try {
+					h263.close();
+				}
+				catch (Exception e) {
+				}
+				h263 = null;
+			}
+			if(avc != null) {
+				try {
+					avc.close();
+				}
+				catch (Exception e) {
+				}
+				avc = null;
+			}
+		}
+	}
+	@Test
+	public void h263avcMultiThreadTest2() {
+		workingFlg = true;
+		FileChannel h263 = null;
+		FileChannel avc = null;
+		final IConverter converter = ConverterFactory.createConverter(new BufferedImage(320, 240, BufferedImage.TYPE_3BYTE_BGR), IPixelFormat.Type.YUV420P);
+		try {
+			final LinkedBlockingQueue<VideoData> h263VideoData = new LinkedBlockingQueue<VideoData>();
+			final LinkedBlockingQueue<VideoData> avcVideoData = new LinkedBlockingQueue<VideoData>();
+			h263 = new FileOutputStream("h263_c.flv").getChannel();
+			avc = new FileOutputStream("avc_c.flv").getChannel();
+			FlvHeader flvHeader = new FlvHeader();
+			flvHeader.setVideoFlg(true);
+			flvHeader.setAudioFlg(false);
+			h263.write(flvHeader.getBuffer());
+			avc.write(flvHeader.getBuffer());
+			
+			final FileChannel h263Channel = h263;
+			final FileChannel avcChannel = avc;
+			Thread h263Thread = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					IStreamCoder encoder = null;
+					try {
+						encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_FLV1);
+						IRational frameRate = IRational.make(15, 1); // 15fps
+						encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+						encoder.setBitRate(250000); // 250kbps
+						encoder.setBitRateTolerance(9000);
+						encoder.setPixelType(IPixelFormat.Type.YUV420P);
+						encoder.setWidth(320);
+						encoder.setHeight(240);
+						encoder.setGlobalQuality(10);
+						encoder.setFrameRate(frameRate);
+						encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+						if(encoder.open(null, null) < 0) {
+							throw new RuntimeException("エンコーダーが開けませんでした。");
+						}
+						IPacket packet = IPacket.make();
+						FlvDepacketizer depacketizer = new FlvDepacketizer();
+						while(workingFlg || h263VideoData.size() > 0) {
+							VideoData vData = h263VideoData.take();
+							IVideoPicture picture = converter.toPicture(vData.getImage(), vData.getTimestamp() * 1000);
+							System.out.println("h263コンバート");
+							if(encoder.encodeVideo(packet, picture, 0) < 0) {
+								throw new Exception("エンコード失敗");
+							}
+							if(packet.isComplete()) {
+								for(Tag tag : depacketizer.getTag(encoder, packet)) {
+									h263Channel.write(tag.getBuffer());
+								}
+							}
+						}
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					finally {
+						if(encoder != null) {
+							encoder.close();
+							encoder = null;
+						}
+					}
+				}
+			});
+			h263Thread.setName("h263ConvertThread");
+			h263Thread.start();
+			Thread avcThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					IStreamCoder encoder = null;
+					try {
+						encoder = IStreamCoder.make(Direction.ENCODING, ICodec.ID.CODEC_ID_H264);
+						IRational frameRate = IRational.make(15, 1); // 15fps
+						encoder.setNumPicturesInGroupOfPictures(30); // gopを30にしておく。keyframeが30枚ごとになる。
+						
+						encoder.setBitRate(250000); // 250kbps
+						encoder.setBitRateTolerance(9000);
+						encoder.setPixelType(IPixelFormat.Type.YUV420P);
+						encoder.setWidth(320);
+						encoder.setHeight(240);
+						encoder.setGlobalQuality(10);
+						encoder.setFrameRate(frameRate);
+						encoder.setTimeBase(IRational.make(1, 1000)); // 1/1000設定(flvはこうなるべき)
+						encoder.setProperty("level", "30");
+						encoder.setProperty("coder", "0");
+						encoder.setProperty("qmin", "10");
+						encoder.setProperty("bf", "0");
+						encoder.setProperty("wprefp", "0");
+						encoder.setProperty("cmp", "+chroma");
+						encoder.setProperty("partitions", "-parti8x8+parti4x4+partp8x8+partp4x4-partb8x8");
+						encoder.setProperty("me_method", "hex");
+						encoder.setProperty("subq", "5");
+						encoder.setProperty("me_range", "16");
+						encoder.setProperty("keyint_min", "25");
+						encoder.setProperty("sc_threshold", "40");
+						encoder.setProperty("i_qfactor", "0.71");
+						encoder.setProperty("b_strategy", "0");
+						encoder.setProperty("qcomp", "0.6");
+						encoder.setProperty("qmax", "30");
+						encoder.setProperty("qdiff", "4");
+						encoder.setProperty("directpred", "0");
+						encoder.setProperty("cqp", "0");
+						encoder.setFlag(Flags.FLAG_LOOP_FILTER, true);
+						encoder.setFlag(Flags.FLAG_CLOSED_GOP, true);
+						if(encoder.open(null, null) < 0) {
+							throw new RuntimeException("エンコーダーが開けませんでした。");
+						}
+						IPacket packet = IPacket.make();
+						FlvDepacketizer depacketizer = new FlvDepacketizer();
+						while(workingFlg || avcVideoData.size() > 0) {
+							VideoData vData = avcVideoData.take();
+							IVideoPicture picture = converter.toPicture(vData.getImage(), vData.getTimestamp() * 1000);
+							if(encoder.encodeVideo(packet, picture, 0) < 0) {
+								throw new Exception("エンコード失敗");
+							}
+							if(packet.isComplete()) {
+								System.out.println("packet complete");
+								for(Tag tag : depacketizer.getTag(encoder, packet)) {
+									avcChannel.write(tag.getBuffer());
+								}
+							}
+						}
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					finally {
+						if(encoder != null) {
+							encoder.close();
+							encoder = null;
+						}
+					}
+				}
+			});
+			avcThread.setName("avcConvertThread");
+			avcThread.start();
+			long startTime = -1;
+			while(true) {
+				long now = System.currentTimeMillis();
+				if(startTime == -1) {
+					startTime = now;
+				}
+				if(now - startTime > 3000) {
+					workingFlg = false;
+					break;
+				}
+				Thread.sleep(5);
+				BufferedImage image = image();
+				h263VideoData.add(new VideoData(image, (now - startTime)));
+				avcVideoData.add(new VideoData(image, (now - startTime)));
+			}
+			System.out.println("処理おわり、thread完了待ち");
+//			h263Thread.interrupt();
+//			avcThread.interrupt();
+			h263Thread.join();
+			avcThread.join();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(h263 != null) {
+				try {
+					h263.close();
+				}
+				catch (Exception e) {
+				}
+				h263 = null;
+			}
+			if(avc != null) {
+				try {
+					avc.close();
+				}
+				catch (Exception e) {
+				}
+				avc = null;
 			}
 		}
 	}
