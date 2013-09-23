629e112ec6f8825dddc910a2925f61f7d6560dad
taktod <poepoemix@hotmail.com>
2013-09-23 19:26:24 +0900

    テスト実行時に他のテスト用にファイルを生成するプログラムを書いてみた。 とりあえずこいつにデータを作らせてから、他のテストが走ればテスト用のファイルの準備でわちゃわちゃすることもないだろう。

---
+++ myLib.setup/src/test/java/com/ttProject/myLib/setup/SetupForTest.java
@@ -0,0 +1,17 @@
+package com.ttProject.myLib.setup;
+
+import org.junit.Test;
+
+/**
+ * ここでセットアップを実行します。
+ * 他のプロジェクト用のテストメディアデータも自動作成したいところ。
+ * ただし、他のプロジェクトライブラリはほぼ使わない予定
+ * @author taktod
+ *
+ */
+public class SetupForTest {
+	@Test
+	public void setup() {
+		System.out.println("セットアップ実行");
+	}
+}
