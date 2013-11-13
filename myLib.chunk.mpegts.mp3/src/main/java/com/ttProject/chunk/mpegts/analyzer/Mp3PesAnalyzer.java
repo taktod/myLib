7771849757c2a5a43d858f2f49c0e9ff00a911ae
taktod_win8 <poepoemix@hotmail.com>
2013-11-14 06:44:50 +0900

    aacとmp3のプロジェクトを追加した。

---
+++ myLib.chunk.mpegts.mp3/src/main/java/com/ttProject/chunk/mpegts/analyzer/Mp3PesAnalyzer.java
@@ -0,0 +1,32 @@
+package com.ttProject.chunk.mpegts.analyzer;
+
+import com.ttProject.chunk.mpegts.AudioDataList;
+import com.ttProject.chunk.mpegts.VideoDataList;
+import com.ttProject.media.Unit;
+
+/**
+ * mp3のframeをpes用に登録します。
+ * 映像用のPesはスルー
+ * @author taktod
+ */
+public class Mp3PesAnalyzer implements IPesAnalyzer {
+
+	@Override
+	public void setVideoDataList(VideoDataList videoDataList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAudioDataList(AudioDataList audioDataList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void analyze(Unit unit) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
