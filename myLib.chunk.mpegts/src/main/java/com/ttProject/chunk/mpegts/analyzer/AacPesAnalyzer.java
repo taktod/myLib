a530ffdee68ce1fdcc376ca8252e528d06aae837
taktod_win8 <poepoemix@hotmail.com>
2013-11-14 18:26:09 +0900

    update

---
+++ myLib.chunk.mpegts/src/main/java/com/ttProject/chunk/mpegts/analyzer/AacPesAnalyzer.java
@@ -0,0 +1,46 @@
+package com.ttProject.chunk.mpegts.analyzer;
+
+import org.apache.log4j.Logger;
+
+import com.ttProject.chunk.mpegts.AudioDataList;
+import com.ttProject.chunk.mpegts.VideoDataList;
+import com.ttProject.media.Unit;
+import com.ttProject.media.aac.frame.Aac;
+
+/**
+ * aacのframeをpes用に登録します。
+ * 映像用のPesはスルー
+ * @author taktod
+ */
+public class AacPesAnalyzer implements IPesAnalyzer {
+	/** 動作ロガー */
+	@SuppressWarnings("unused")
+	private Logger logger = Logger.getLogger(AacPesAnalyzer.class);
+	/** audioのデータ保持オブジェクト */
+	private AudioDataList audioDataList;
+	/**
+	 * videoDataListを設定します。
+	 */
+	@Override
+	public void setVideoDataList(VideoDataList videoDataList) {
+		// 処理することがないので捨てます
+	}
+	/**
+	 * audioDataListを設定します。
+	 */
+	@Override
+	public void setAudioDataList(AudioDataList audioDataList) {
+		this.audioDataList = audioDataList;
+	}
+	/**
+	 * データを登録します。開始時のptsは0ととりあえずしておきます。
+	 */
+	@Override
+	public void analyze(Unit unit) {
+		if(unit instanceof Aac) {
+			// aacの場合はaudioDataListにいれますが、動作中のptsが必要になります。(開始位置の情報を登録する必要があるります。)
+			Aac aac = (Aac) unit;
+			audioDataList.addAudioData(aac, 0);
+		}
+	}
+}
---
+++ myLib.chunk.mpegts/src/main/java/com/ttProject/chunk/mpegts/analyzer/Mp3PesAnalyzer.java
@@ -0,0 +1,46 @@
+package com.ttProject.chunk.mpegts.analyzer;
+
+import org.apache.log4j.Logger;
+
+import com.ttProject.chunk.mpegts.AudioDataList;
+import com.ttProject.chunk.mpegts.VideoDataList;
+import com.ttProject.media.Unit;
+import com.ttProject.media.mp3.frame.Mp3;
+
+/**
+ * mp3のframeをpes用に登録します。
+ * 映像用のPesはスルー
+ * @author taktod
+ */
+public class Mp3PesAnalyzer implements IPesAnalyzer {
+	/** 動作ロガー */
+	@SuppressWarnings("unused")
+	private Logger logger = Logger.getLogger(Mp3PesAnalyzer.class);
+	/** audioのデータ保持オブジェクト */
+	private AudioDataList audioDataList;
+	/**
+	 * videoDataListを設定します。
+	 */
+	@Override
+	public void setVideoDataList(VideoDataList videoDataList) {
+		// やることなし
+	}
+	/**
+	 * audioDataListを設定します。
+	 */
+	@Override
+	public void setAudioDataList(AudioDataList audioDataList) {
+		this.audioDataList = audioDataList;
+	}
+	/**
+	 * pmt値が知りたいので登録するようにしておきます。
+	 * 開始時のptsはとりあえず0と仮定します。
+	 */
+	@Override
+	public void analyze(Unit unit) {
+		if(unit instanceof Mp3) {
+			Mp3 mp3 = (Mp3)unit;
+			audioDataList.addAudioData(mp3, 0);
+		}
+	}
+}
