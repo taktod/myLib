7ed72730616e43927c981de20666ed5959c32358
taktod <poepoemix@hotmail.com>
2013-11-16 12:50:29 +0900

    テストコード変更しといた。

--- myLib.media.mpegts/src/test/java/com/ttProject/media/mpegts/test/FileAnalyzeTest.java
+++ myLib.media.mpegts/src/test/java/com/ttProject/media/mpegts/test/FileAnalyzeTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import com.ttProject.media.mpegts.IPacketAnalyzer;
 import com.ttProject.media.mpegts.Packet;
 import com.ttProject.media.mpegts.PacketAnalyzer;
-import com.ttProject.media.mpegts.field.AdaptationField;
 import com.ttProject.media.mpegts.packet.Pes;
+import com.ttProject.media.mpegts.packet.Pmt;
 import com.ttProject.nio.channels.FileReadChannel;
 import com.ttProject.nio.channels.IReadChannel;
 
@@ -23,43 +23,28 @@ public class FileAnalyzeTest {
 	 */
 	@Test
 	public void fixedFileTest() throws Exception {
-		IReadChannel source = FileReadChannel.openFileReadChannel(
-				"/Users/todatakahiko/Documents/workspace_mvn/myLib/myLib.packet.mpegts/test_made.ts"
-		);
-/*		IReadChannel source = FileReadChannel.openFileReadChannel(
-				Thread.currentThread().getContextClassLoader().getResource("mario.ts")
-		);*/
-		IPacketAnalyzer analyzer = new PacketAnalyzer();
-		Packet packet = null;
-		int counter = 0;
-		while((packet = analyzer.analyze(source)) != null) {
-			if(packet instanceof Pes) {
-				Pes pes = (Pes) packet;
-				if(pes.isPayloadUnitStart()) {
-					if(pes.isAdaptationFieldExist()) {
-						AdaptationField aField = pes.getAdaptationField();
-						if(aField.hasPcr()) {
-							logger.info("pcr:" + (aField.getPcrBase() / 90000D));
-							logger.info(pes.getPts());
-						}
-					}
-					if(pes.hasPts()) {
-						if(pes.getPid() == 0x0101) {
-							logger.info(pes.getPesPacketLength());
-							logger.info(counter);
-							counter = 0;
-							logger.info("*" + pes.getPts());
-						}
-						else {
-//							logger.info(pes.getPts());
-						}
-					}
+		try {
+			IReadChannel source = FileReadChannel.openFileReadChannel(
+					Thread.currentThread().getContextClassLoader().getResource("rtypeDelta.ts")
+			);
+			IPacketAnalyzer analyzer = new PacketAnalyzer();
+			Packet packet = null;
+//			int counter = 0;
+			while((packet = analyzer.analyze(source)) != null) {
+				if(packet instanceof Pmt) {
+					System.out.println(packet);
 				}
-				if(pes.getPid() == 0x0101) {
-					counter ++;
+				if(packet instanceof Pes) {
+					Pes pes = (Pes) packet;
+					if(pes.isPayloadUnitStart()) {
+						logger.info(pes);
+					}
 				}
 			}
+			source.close();
 		}
-		source.close();
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
