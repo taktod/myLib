2bcf053294e731c65a6137a121a1d04f7fe0a6a9
taktod <poepoemix@hotmail.com>
2013-11-24 16:41:07 +0900

    Executorの動作について、知りたかったのでテストを書いてみた。

---
+++ myLib.MIT/myLib.transcode.ffmpeg/src/test/java/com/ttProject/transcode/ffmpeg/test/ExecutorLoopTest.java
@@ -0,0 +1,49 @@
+package com.ttProject.transcode.ffmpeg.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.junit.Test;
+
+/**
+ * executorをつかって無限ループする場合の動作テストやってみる。
+ * @author taktod
+ */
+public class ExecutorLoopTest {
+	private int count = 10;
+	@Test
+	public void test() {
+		final ExecutorService exec = Executors.newSingleThreadExecutor();
+		if(exec instanceof ThreadPoolExecutor) {
+			System.out.println(((ThreadPoolExecutor)exec).getActiveCount());
+		}
+		else {
+			System.out.println(exec.getClass());
+		}
+		exec.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(100);
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("here");
+				count --;
+				if(count != 0) {
+					exec.execute(this);
+				}
+			}
+		});
+		try {
+			Thread.sleep(500);
+			exec.shutdown(); // 止めたくなったらこれ以上queueが入らないようにしちゃう。
+		}
+		catch(Exception e) {
+			
+		}
+		System.out.println("here...");
+	}
+}
