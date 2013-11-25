dd0399e390d9e7405af4a81ffe119e7c6a6deb90
taktod_win8 <poepoemix@hotmail.com>
2013-11-26 06:30:20 +0900

    frame動作を作りかけ。 とりあえず、keyFrame、innerFrameの見分けとsizeは計測できるようにしたいですね。

---
+++ myLib.MIT/myLib.media.flv1/src/main/java/com/ttProject/media/flv1/Frame.java
@@ -0,0 +1,10 @@
+package com.ttProject.media.flv1;
+
+/**
+ * flv1のコーデックの映像(H263 sorenson)の内容を解析します。
+ * @see http://hkpr.info/flash/swf/index.php?%E3%83%93%E3%83%87%E3%82%AA%2FSorenson%20H.263%20%E3%83%93%E3%83%83%E3%83%88%E3%82%B9%E3%83%88%E3%83%AA%E3%83%BC%E3%83%A0%E3%83%95%E3%82%A9%E3%83%BC%E3%83%9E%E3%83%83%E3%83%88
+ * @author taktod
+ */
+public class Frame {
+
+}
---
+++ myLib.MIT/myLib.media.vp6/src/main/java/com/ttProject/media/vp6/Frame.java
@@ -0,0 +1,10 @@
+package com.ttProject.media.vp6;
+
+/**
+ * on2Vp6のコーデックの映像の内容を解析します。
+ * @see http://wiki.multimedia.cx/index.php?title=On2_VP6
+ * @author taktod
+ */
+public class Frame {
+
+}
