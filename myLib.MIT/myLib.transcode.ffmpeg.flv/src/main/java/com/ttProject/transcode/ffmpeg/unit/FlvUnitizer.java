6e25cbb8564b76111d7ee9f0f020cace9218df31
taktod <poepoemix@hotmail.com>
2013-11-28 19:34:00 +0900

    ffmpegの変換出力からflvTagを取り出す動作

---
+++ myLib.MIT/myLib.transcode.ffmpeg.flv/src/main/java/com/ttProject/transcode/ffmpeg/unit/FlvUnitizer.java
@@ -0,0 +1,31 @@
+package com.ttProject.transcode.ffmpeg.unit;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import com.ttProject.media.flv.FlvHeader;
+import com.ttProject.media.flv.FlvManager;
+import com.ttProject.nio.channels.ByteReadChannel;
+
+/**
+ * flvのbufferStreamをunitに変換するプログラム
+ * @author taktod
+ */
+public class FlvUnitizer implements IUnitizer {
+	private FlvHeader flvHeader = null;
+	private final FlvManager flvManager = new FlvManager();
+	/**
+	 * 入力データをflvTagに書き換えます。
+	 */
+	@Override
+	public List<?> getUnits(ByteBuffer buffer) throws Exception {
+		if(flvHeader == null) {
+			// 初アクセスの場合はflvHeaderであることを期待します。
+			flvHeader = new FlvHeader();
+			ByteReadChannel channel = new ByteReadChannel(buffer);
+			flvHeader.analyze(channel);
+			buffer.position(13);
+		}
+		return 	flvManager.getUnits(buffer);
+	}
+}
