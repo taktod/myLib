3840cdce924b627bc7e98b8c5cc43ece02102ddd
taktod_win8 <poepoemix@hotmail.com>
2013-12-10 06:36:21 +0900

    frame

--- myLib.MIT/myLib.frame.aac/src/main/java/com/ttProject/frame/aac/AacFrame.java
+++ myLib.MIT/myLib.frame.aac/src/main/java/com/ttProject/frame/aac/AacFrame.java
@@ -1,7 +1,6 @@
 package com.ttProject.frame.aac;
 
 import com.ttProject.frame.AudioFrame;
-import com.ttProject.nio.channels.IReadChannel;
 
 public abstract class AacFrame extends AudioFrame {
 }
--- myLib.MIT/myLib.frame.aac/src/main/java/com/ttProject/frame/aac/DecoderSpecificInfo.java
+++ myLib.MIT/myLib.frame.aac/src/main/java/com/ttProject/frame/aac/DecoderSpecificInfo.java
@@ -3,20 +3,22 @@ package com.ttProject.frame.aac;
 import com.ttProject.unit.extra.Bit1;
 import com.ttProject.unit.extra.Bit4;
 import com.ttProject.unit.extra.Bit5;
+import com.ttProject.unit.extra.Bit6;
+import com.ttProject.unit.extra.BitN.Bit24;
 
 /**
  * aacのdecode specific infoのデータから
  * @see http://wiki.multimedia.cx/index.php?title=MPEG-4_Audio
+ * これ・・・扱い的にはglobalHeaderらしい
  * @author taktod
  */
 public class DecoderSpecificInfo {
-	private Bit5 objectType1; // profileの事
-//	private Bit6 objectType2;
-	private Bit4 frequencyIndex; // samplingFrequenceIndexと同じ
-//	private int frequency; // 24bit indexが0x0Fの場合
-	private Bit4 channelConfiguration;
-	private Bit1 frameLengthFlag; // 0:each packetcontains 1024 samples 1:960 samples
-	private Bit1 dependsOnCoreCoder;
-	private Bit1 extensionFlag;
-
+	private Bit5 objectType1 = new Bit5(); // profileの事
+	private Bit6 objectType2 = null; // objectTypeが31の場合
+	private Bit4 frequencyIndex = new Bit4(); // samplingFrequenceIndexと同じ
+	private Bit24 frequency = null; // indexが15の場合
+	private Bit4 channelConfiguration = new Bit4();
+	private Bit1 frameLengthFlag = new Bit1(); // 0:each packetcontains 1024 samples 1:960 samples
+	private Bit1 dependsOnCoreCoder = new Bit1();
+	private Bit1 extensionFlag = new Bit1();
 }
