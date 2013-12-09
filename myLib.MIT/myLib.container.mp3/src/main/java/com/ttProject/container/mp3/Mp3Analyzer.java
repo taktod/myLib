38a286c9652abf548e23e6629611fa259f821171
taktod <poepoemix@hotmail.com>
2013-12-09 19:39:30 +0900

    mp3のコンテナ動作はmp3frameの同等なので、そちらのデータをひっぱってきておわりにした。

---
+++ myLib.MIT/myLib.container.mp3/src/main/java/com/ttProject/container/mp3/Mp3Analyzer.java
@@ -0,0 +1,7 @@
+package com.ttProject.container.mp3;
+
+import com.ttProject.frame.mp3.Mp3FrameAnalyzer;
+import com.ttProject.unit.IAnalyzer;
+
+public class Mp3Analyzer extends Mp3FrameAnalyzer implements IAnalyzer {
+}
---
+++ myLib.MIT/myLib.container.mp3/src/main/java/com/ttProject/container/mp3/Mp3Selector.java
@@ -0,0 +1,7 @@
+package com.ttProject.container.mp3;
+
+import com.ttProject.frame.mp3.Mp3FrameSelector;
+import com.ttProject.unit.ISelector;
+
+public class Mp3Selector extends Mp3FrameSelector implements ISelector {
+}
