a2324b6384de64753fbb0b580d8616acf52f2ae0
taktod <poepoemix@hotmail.com>
2013-12-13 19:12:21 +0900

    nal解析とdataNal解析を書いてみた。 あとで利用する予定。
    nal解析は、もしかしたらバグがあるかも・・・という予感あり。

---
+++ myLib.MIT/myLib.frame.h264/src/main/java/com/ttProject/frame/h264/DataNalAnalyzer.java
@@ -0,0 +1,38 @@
+package com.ttProject.frame.h264;
+
+import com.ttProject.nio.channels.ByteReadChannel;
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.unit.IAnalyzer;
+import com.ttProject.unit.ISelector;
+import com.ttProject.unit.IUnit;
+import com.ttProject.util.BufferUtil;
+
+/**
+ * flvやh264の様なsize + dataのnalを解析する動作
+ * 実体の読み込みまで実施します。
+ * @author taktod
+ */
+public class DataNalAnalyzer implements IAnalyzer {
+	/** セレクター */
+	private ISelector selector = new H264FrameSelector();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public IUnit analyze(IReadChannel channel) throws Exception {
+		if(channel.size() < 4) {
+			throw new Exception("読み込みバッファ量がおかしいです。");
+		}
+		int size = BufferUtil.safeRead(channel, 4).getInt();
+		if(size <= 0) {
+			throw new Exception("データ指定がおかしいです。");
+		}
+		if(channel.size() - channel.position() < size) {
+			throw new Exception("データが足りません");
+		}
+		IReadChannel byteChannel = new ByteReadChannel(BufferUtil.safeRead(channel, size));
+		IUnit unit = selector.select(byteChannel);
+		unit.load(byteChannel);
+		return unit;
+	}
+}
---
+++ myLib.MIT/myLib.frame.h264/src/main/java/com/ttProject/frame/h264/NalAnalyzer.java
@@ -0,0 +1,118 @@
+package com.ttProject.frame.h264;
+
+import java.nio.ByteBuffer;
+
+import com.ttProject.nio.channels.ByteReadChannel;
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.unit.IAnalyzer;
+import com.ttProject.unit.ISelector;
+import com.ttProject.unit.IUnit;
+import com.ttProject.util.BufferUtil;
+
+/**
+ * mpegtsのような00 00 01 + dataのnalを解析する動作
+ * 実体の読み込みまで実施します。
+ * @author taktod
+ */
+public class NalAnalyzer implements IAnalyzer {
+	/** selector */
+	private ISelector selector = new H264FrameSelector();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public IUnit analyze(IReadChannel channel) throws Exception {
+		Short lastData = null;
+		ByteBuffer buffer = ByteBuffer.allocate(channel.size() - channel.position());
+		// データを読み込んでいく
+		while(channel.size() - channel.position() > 1) {
+			short data = BufferUtil.safeRead(channel, 2).getShort();
+			// 00 00 00 01もしくは 00 00 01がnalの分岐点
+			// よってshort = 0になった場合に注意して処理すればいい
+			if(data == 0) {
+				byte firstByte, secondByte;
+				firstByte = BufferUtil.safeRead(channel, 1).get();
+				if(firstByte == 1) {
+					checkLastData(buffer, lastData);
+					buffer.flip();
+					if(buffer.remaining() == 0) {
+						buffer = ByteBuffer.allocate(channel.size() - channel.position());
+						continue;
+					}
+					return setupFrame(buffer);
+				}
+				else if(firstByte == 0) {
+					secondByte = BufferUtil.safeRead(channel, 1).get();
+					if(secondByte == 1) {
+						checkLastData(buffer, lastData);
+						buffer.flip();
+						if(buffer.remaining() == 0) {
+							buffer = ByteBuffer.allocate(channel.size() - channel.position());
+							continue;
+						}
+						return setupFrame(buffer);
+					}
+					else {
+						if(lastData != null) {
+							buffer.putShort(lastData);
+						}
+						buffer.putShort(data);
+						buffer.put(firstByte);
+						buffer.put(secondByte);
+					}
+				}
+				else {
+					if(lastData != null) {
+						buffer.putShort(lastData);
+					}
+					buffer.putShort(data);
+					buffer.put(firstByte);
+				}
+				lastData = null;
+			}
+			else { // 0ではない
+				if(lastData != null && data == 1 && (lastData & 0x00FF) == 0) {
+					checkLastData(buffer, lastData);
+					buffer.flip();
+					if(buffer.remaining() == 0) {
+						buffer = ByteBuffer.allocate(channel.size() - channel.position());
+						continue;
+					}
+					return setupFrame(buffer);
+				}
+				setLastData(buffer, lastData);
+				lastData = data;
+			}
+		}
+		setLastData(buffer, lastData);
+		if(channel.size() - channel.position() == 1) {
+			buffer.put(BufferUtil.safeRead(channel, 1).get());
+		}
+		buffer.flip();
+		if(buffer.remaining() == 0) {
+			return null;
+		}
+		return setupFrame(buffer);
+	}
+	private IUnit setupFrame(ByteBuffer buffer) throws Exception {
+		IReadChannel channel = new ByteReadChannel(buffer);
+		IUnit unit = selector.select(channel);
+		unit.load(channel);
+		return unit;
+	}
+	private void setLastData(ByteBuffer buffer, Short lastData) {
+		if(lastData != null) {
+			buffer.putShort(lastData);
+		}
+	}
+	private void checkLastData(ByteBuffer buffer, Short lastData) {
+		if(lastData != null) {
+			if((lastData & 0x00FF) == 0) {
+				buffer.put((byte)(lastData >>> 8));
+			}
+			else {
+				buffer.putShort(lastData);
+			}
+		}
+	}
+}
