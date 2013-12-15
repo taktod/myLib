70296bf5f878bf2f4e2524660b82183a57837032
taktod <poepoemix@hotmail.com>
2013-12-15 13:44:08 +0900

    調整した。

--- myLib.MIT/myLib.frame.adpcmswf/src/main/java/com/ttProject/frame/adpcmswf/AdpcmswfFrame.java
+++ myLib.MIT/myLib.frame.adpcmswf/src/main/java/com/ttProject/frame/adpcmswf/AdpcmswfFrame.java
@@ -1,7 +1,6 @@
 package com.ttProject.frame.adpcmswf;
 
 import com.ttProject.frame.AudioFrame;
-import com.ttProject.nio.channels.IReadChannel;
 
 public abstract class AdpcmswfFrame extends AudioFrame {
 }
--- myLib.MIT/myLib.frame.adpcmswf/src/main/java/com/ttProject/frame/adpcmswf/AdpcmswfFrameAnalyzer.java
+++ myLib.MIT/myLib.frame.adpcmswf/src/main/java/com/ttProject/frame/adpcmswf/AdpcmswfFrameAnalyzer.java
@@ -4,12 +4,16 @@ import com.ttProject.nio.channels.IReadChannel;
 import com.ttProject.unit.IAnalyzer;
 import com.ttProject.unit.IUnit;
 
+/**
+ * adpcmswfのframeを解析する動作
+ * @author taktod
+ */
 public class AdpcmswfFrameAnalyzer implements IAnalyzer {
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public IUnit analyze(IReadChannel channel) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
