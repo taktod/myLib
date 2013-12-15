b3564e09eba939dd4b2ee9c8deebfed66f48a787
taktod <poepoemix@hotmail.com>
2013-12-15 15:44:17 +0900

    vp8も準備だけやっとく。

--- myLib.MIT/myLib.frame.vp8/src/main/java/com/ttProject/frame/vp8/Vp8FrameAnalyzer.java
+++ myLib.MIT/myLib.frame.vp8/src/main/java/com/ttProject/frame/vp8/Vp8FrameAnalyzer.java
@@ -1,14 +1,22 @@
 package com.ttProject.frame.vp8;
 
+import com.ttProject.frame.VideoSelector;
 import com.ttProject.nio.channels.IReadChannel;
 import com.ttProject.unit.IAnalyzer;
 import com.ttProject.unit.IUnit;
 
 public class Vp8FrameAnalyzer implements IAnalyzer {
-
+	private VideoSelector selector = new Vp8FrameSelector();
+	public VideoSelector getSelector() {
+		return selector;
+	}
 	@Override
 	public IUnit analyze(IReadChannel channel) throws Exception {
-		return null;
+		IUnit unit = selector.select(channel);
+		if(unit != null) {
+			unit.load(channel);
+		}
+		return unit;
 	}
 
 }
--- myLib.MIT/myLib.frame.vp8/src/main/java/com/ttProject/frame/vp8/Vp8FrameSelector.java
+++ myLib.MIT/myLib.frame.vp8/src/main/java/com/ttProject/frame/vp8/Vp8FrameSelector.java
@@ -1,10 +1,10 @@
 package com.ttProject.frame.vp8;
 
+import com.ttProject.frame.VideoSelector;
 import com.ttProject.nio.channels.IReadChannel;
-import com.ttProject.unit.ISelector;
 import com.ttProject.unit.IUnit;
 
-public class Vp8FrameSelector implements ISelector {
+public class Vp8FrameSelector extends VideoSelector {
 
 	@Override
 	public IUnit select(IReadChannel channel) throws Exception {
