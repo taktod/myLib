7b7e456b3544b4e0ad38982d6c1a8fdb5087ecc2
taktod <poepoemix@hotmail.com>
2013-12-21 08:21:59 +0900

    xuggleのframe用プロジェクトを再度つくりました。 こっちの方が都合がよさそう。

---
+++ myLib.GPLv3/myLib.xuggle.frame/src/main/java/com/ttProject/xuggle/frame/Packetizer.java
@@ -0,0 +1,14 @@
+package com.ttProject.xuggle.frame;
+
+import com.ttProject.frame.IFrame;
+import com.xuggle.xuggler.IPacket;
+
+/**
+ * flameデータをxuggleのpacketに変換するプログラム
+ * @author taktod
+ */
+public class Packetizer {
+	public IPacket getPacket(IFrame frame) {
+		return null;
+	}
+}
