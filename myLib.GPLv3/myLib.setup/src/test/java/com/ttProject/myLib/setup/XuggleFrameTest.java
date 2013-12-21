ff1d40ee063230b5359da7f939cdce92795a4e42
taktod_win8 <poepoemix@hotmail.com>
2013-12-22 08:52:07 +0900

    mp3の動作の調整を進める。 flvとmp3のコンテナで動作可能とする。(本当はmkvやmp4もやりたい。)

--- myLib.GPLv3/myLib.setup/src/test/java/com/ttProject/myLib/setup/XuggleFrameTest.java
+++ myLib.GPLv3/myLib.setup/src/test/java/com/ttProject/myLib/setup/XuggleFrameTest.java
@@ -18,12 +18,33 @@ public class XuggleFrameTest extends SetupBase {
 	 */
 	@Test
 	public void flv1() throws Exception {
-		logger.info("flv1準備");
+		logger.info("flv1(flv)準備");
 		init();
 		IContainer container = IContainer.make();
 		if(container.open(getTargetFile("myLib.GPLv3/myLib.xuggle.test", "flv1.flv"), IContainer.Type.WRITE, null) < 0) {
 			throw new Exception("ファイルが開けませんでした");
 		}
 		processConvert(container, Encoder.flv1(container), null);
+	}
+	/**
+	 * mp3用の検証用データ
+	 * @throws Exception
+	 */
+	@Test
+	public void mp3() throws Exception {
+		logger.info("mp3(flv)準備");
+		init();
+		IContainer container = IContainer.make();
+		if(container.open(getTargetFile("myLib.GPLv3/myLib.xuggle.test", "mp3.flv"), IContainer.Type.WRITE, null) < 0) {
+			throw new Exception("ファイルが開けませんでした");
+		}
+		processConvert(container, null, Encoder.mp3(container));
+		logger.info("mp3(mp3)準備");
+		init();
+		container = IContainer.make();
+		if(container.open(getTargetFile("myLib.GPLv3/myLib.xuggle.test", "mp3.mp3"), IContainer.Type.WRITE, null) < 0) {
+			throw new Exception("ファイルが開けませんでした");
+		}
+		processConvert(container, null, Encoder.mp3(container));
 	}
 }
