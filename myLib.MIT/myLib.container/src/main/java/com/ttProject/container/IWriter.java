c38899017520606a6b8855b575a94cb43d0f1240
taktod <poepoemix@hotmail.com>
2013-12-25 09:35:13 +0900

    writerもつくってみようと思う。 とりあえず、flvからいってみようか・・・ なおwriterは共通項目ある必要はないと思う。

--- myLib.MIT/myLib.container/src/main/java/com/ttProject/container/IWriter.java
+++ myLib.MIT/myLib.container/src/main/java/com/ttProject/container/IWriter.java
@@ -1,9 +1,31 @@
 package com.ttProject.container;
 
+import com.ttProject.frame.IFrame;
+
 /**
  * ファイル書き込み
+ * コンテナからも作成可能にするが、それ以上にframeから作成可能にしておきたい
  * @author taktod
  */
 public interface IWriter {
-
+	/**
+	 * コンテナデータを追加
+	 * いきなり非推奨って・・・
+	 * @param container
+	 */
+	@Deprecated
+	public void addContainer(IContainer container);
+	/**
+	 * メディアフレームを追加
+	 * @param frame
+	 */
+	public void addFrame(IFrame frame);
+	/**
+	 * headerデータを構築
+	 */
+	public void prepareHeader();
+	/**
+	 * 終端データを構築
+	 */
+	public void prepareTailer();
 }
