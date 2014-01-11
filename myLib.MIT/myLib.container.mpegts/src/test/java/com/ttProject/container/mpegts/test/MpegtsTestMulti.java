d6ec967267b0777ccce93700962e9fbb4380c4c6
taktod <poepoemix@hotmail.com>
2014-01-11 12:37:19 +0900

    並列して、3video 1audioのmpegtsから 1video 1audio x 3のmpegtsを作るプログラムを書いてみた。

---
+++ myLib.MIT/myLib.container.mpegts/src/test/java/com/ttProject/container/mpegts/test/MpegtsTestMulti.java
@@ -0,0 +1,114 @@
+package com.ttProject.container.mpegts.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.ttProject.container.IContainer;
+import com.ttProject.container.IReader;
+import com.ttProject.container.mpegts.CodecType;
+import com.ttProject.container.mpegts.MpegtsPacketReader;
+import com.ttProject.container.mpegts.MpegtsPacketWriter;
+import com.ttProject.container.mpegts.field.PmtElementaryField;
+import com.ttProject.container.mpegts.field.PmtElementaryFieldFactory;
+import com.ttProject.container.mpegts.type.Pat;
+import com.ttProject.container.mpegts.type.Pes;
+import com.ttProject.container.mpegts.type.Pmt;
+import com.ttProject.container.mpegts.type.Sdt;
+import com.ttProject.nio.channels.FileReadChannel;
+import com.ttProject.nio.channels.IFileReadChannel;
+
+/**
+ * mpegtsの動作テスト
+ * @author taktod
+ */
+public class MpegtsTestMulti {
+	/** ロガー */
+	private Logger logger = Logger.getLogger(MpegtsTestMulti.class);
+	@Test
+	public void test() throws Exception {
+		logger.info("test");
+		analyzerTest(
+			FileReadChannel.openFileReadChannel(
+					"http://49.212.39.17/mario_3video_1audio.ts" // ソースとして、3つのvideoトラック 1つのaudioトラックをもつmpegtsのデータをあてておく
+			)
+		);
+	}
+	private void analyzerTest(IFileReadChannel source) {
+		MpegtsPacketWriter writer1 = null;
+		MpegtsPacketWriter writer2 = null;
+		MpegtsPacketWriter writer3 = null;
+		PmtElementaryFieldFactory factory = new PmtElementaryFieldFactory();
+		try {
+			writer1 = new MpegtsPacketWriter("output_640x360.ts");
+			writer2 = new MpegtsPacketWriter("output_320x180.ts");
+			writer3 = new MpegtsPacketWriter("output_160x90.ts");
+			Sdt sdt = new Sdt();
+			sdt.writeDefaultProvider("test", "hpgehoge");
+			writer1.addContainer(sdt);
+			writer2.addContainer(sdt);
+			writer3.addContainer(sdt);
+			Pat pat = new Pat();
+			writer1.addContainer(pat);
+			writer2.addContainer(pat);
+			writer3.addContainer(pat);
+			Pmt pmt = new Pmt(pat.getPmtPid());
+			PmtElementaryField videoElementaryField = factory.makeNewField(CodecType.VIDEO_H264);
+			pmt.addNewField(videoElementaryField);
+			pmt.setPcrPid(videoElementaryField.getPid());
+			PmtElementaryField audioElementaryField = factory.makeNewField(CodecType.AUDIO_AAC);
+			pmt.addNewField(audioElementaryField);
+			writer1.addContainer(pmt);
+			writer2.addContainer(pmt);
+			writer3.addContainer(pmt);
+			
+			IReader reader = new MpegtsPacketReader();
+			IContainer container = null;
+			while((container = reader.read(source)) != null) {
+				if(container instanceof Pes) {
+					Pes pes = (Pes)container;
+					switch(pes.getPid()) {
+					case 0x0100: // track1
+						if(pes.getFrame() != null) {
+							writer1.addFrame(videoElementaryField.getPid(), pes.getFrame());
+						}
+						break;
+					case 0x0101:
+						if(pes.getFrame() != null) {
+							writer2.addFrame(videoElementaryField.getPid(), pes.getFrame());
+						}
+						break;
+					case 0x0102:
+						if(pes.getFrame() != null) {
+							writer3.addFrame(videoElementaryField.getPid(), pes.getFrame());
+						}
+						break;
+					case 0x0103:
+						if(pes.getFrame() != null) {
+							writer1.addFrame(audioElementaryField.getPid(), pes.getFrame());
+							writer2.addFrame(audioElementaryField.getPid(), pes.getFrame());
+							writer3.addFrame(audioElementaryField.getPid(), pes.getFrame());
+						}
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			writer1.prepareTailer();
+			writer2.prepareTailer();
+			writer3.prepareTailer();
+		}
+		catch(Exception e) {
+			logger.warn("例外発生", e);
+		}
+		finally {
+			if(source != null) {
+				try {
+					source.close();
+				}
+				catch(Exception e) {}
+				source = null;
+			}
+		}
+	}
+}
