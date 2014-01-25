a5f59d66d2b66cd9374f75d6517880903248f461
taktod_win8 <poepoemix@hotmail.com>
2014-01-25 15:08:10 +0900

    vorbis用の処理作りかけ。

---
+++ myLib.MIT/myLib.frame.vorbis/src/main/java/com/ttProject/frame/vorbis/VorbisHeaderBufferSplitter.java
@@ -0,0 +1,30 @@
+package com.ttProject.frame.vorbis;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.util.BufferUtil;
+
+/**
+ * vorbisのheaderBufferを分割して、ByteBufferのリストとして応答します。
+ * @author taktod
+ */
+public class VorbisHeaderBufferSplitter {
+	public List<ByteBuffer> doSplit(IReadChannel channel) throws Exception {
+		List<ByteBuffer> result = new ArrayList<ByteBuffer>();
+		int num = BufferUtil.safeRead(channel, 1).get();
+		List<Integer> sizeList = new ArrayList<Integer>();
+		for(int i = 0;i < num;i ++) {
+			sizeList.add((int)(BufferUtil.safeRead(channel, 1).get()));
+		}
+		for(Integer size : sizeList) {
+			ByteBuffer buffer = BufferUtil.safeRead(channel, size);
+			result.add(buffer);
+		}
+		ByteBuffer buffer = BufferUtil.safeRead(channel, channel.size() - channel.position());
+		result.add(buffer);
+		return result;
+	}
+}
