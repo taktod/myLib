811d108818623cfef961a376b81495f3f6c6f2fd
taktod <poepoemix@hotmail.com>
2014-02-02 23:37:54 +0900

    あとは、このクラスを改良すれば、mpegtsの動作はなんとかできそう。

--- myLib.MIT/myLib.container.mpegts/src/main/java/com/ttProject/container/mpegts/MpegtsPacketReader.java
+++ myLib.MIT/myLib.container.mpegts/src/main/java/com/ttProject/container/mpegts/MpegtsPacketReader.java
@@ -1,13 +1,34 @@
 package com.ttProject.container.mpegts;
 
+import com.ttProject.container.IContainer;
 import com.ttProject.container.Reader;
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.unit.IUnit;
 
 /**
  * mpegtsPacketを解析します。
  * @author taktod
  */
 public class MpegtsPacketReader extends Reader {
+	/**
+	 * コンストラクタ
+	 */
 	public MpegtsPacketReader() {
 		super(new MpegtsPacketSelector());
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public IContainer read(IReadChannel channel) throws Exception {
+		IUnit unit = getSelector().select(channel);
+		if(unit != null) {
+			unit.load(channel);
+		}
+		// TODO ここに細工を実施します。
+		/*
+		 * 未完了pesの場合はNullUnitを応答します。
+		 */
+		return (IContainer) unit;
 	}
 }
