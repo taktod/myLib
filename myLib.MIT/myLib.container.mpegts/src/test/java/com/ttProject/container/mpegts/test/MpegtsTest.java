204c6d2656d32833b8dc78b3d9c9eb65bc65fa54
taktod <poepoemix@hotmail.com>
2014-02-03 22:33:40 +0900

    動作テストを追加した。(iPhone5Sで録画したおおきなnalの動作)

--- myLib.MIT/myLib.container.mpegts/src/test/java/com/ttProject/container/mpegts/test/MpegtsTest.java
+++ myLib.MIT/myLib.container.mpegts/src/test/java/com/ttProject/container/mpegts/test/MpegtsTest.java
@@ -7,6 +7,9 @@ import com.ttProject.container.IContainer;
 import com.ttProject.container.IReader;
 import com.ttProject.container.mpegts.MpegtsPacketReader;
 import com.ttProject.container.mpegts.type.Pes;
+import com.ttProject.frame.IFrame;
+import com.ttProject.frame.IVideoFrame;
+import com.ttProject.frame.extra.VideoMultiFrame;
 import com.ttProject.nio.channels.FileReadChannel;
 import com.ttProject.nio.channels.IFileReadChannel;
 
@@ -17,6 +20,10 @@ import com.ttProject.nio.channels.IFileReadChannel;
 public class MpegtsTest {
 	/** ロガー */
 	private Logger logger = Logger.getLogger(MpegtsTest.class);
+	/**
+	 * 通常のフレームの動作テスト
+	 * @throws Exception
+	 */
 	@Test
 	public void test() throws Exception {
 		logger.info("test");
@@ -26,18 +33,38 @@ public class MpegtsTest {
 			)
 		);
 	}
+	/**
+	 * iphone5Sで録画したnalの非常におおきなデータ
+	 * 動作はするけど、遅すぎます
+	 * @throws Exception
+	 */
+	@Test
+	public void largeSliceTest() throws Exception {
+		logger.info("largeSliceTest");
+		analyzerTest(
+			FileReadChannel.openFileReadChannel(
+					"http://49.212.39.17/ahiru.ts"
+			)
+		);
+	}
 	private void analyzerTest(IFileReadChannel source) {
 		try {
 			IReader reader = new MpegtsPacketReader();
 			IContainer container = null;
 			while((container = reader.read(source)) != null) {
 				if(container instanceof Pes) {
+					Pes pes = (Pes) container;
 					// 再終端まできていないとデータがきません。
-					if(((Pes) container).getFrame() != null) {
-						logger.info(((Pes) container).getFrame());
+					IFrame frame = pes.getFrame();
+					if(frame != null && frame instanceof IVideoFrame) {
+						logger.info("pesFrame");
+						if(frame instanceof VideoMultiFrame) {
+							for(IVideoFrame vFrame : ((VideoMultiFrame) frame).getFrameList()) {
+								logger.info(vFrame + ":" + vFrame.getSize() + "bytes" + " " + (vFrame.getPts() + 0) + " pts" + " " + vFrame.getDts() + " dts");
+							}
+						}
 					}
 				}
-//				logger.info(container);
 			}
 		}
 		catch(Exception e) {
