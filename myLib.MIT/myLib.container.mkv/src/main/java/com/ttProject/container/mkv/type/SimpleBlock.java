e23ec0c6d34a5621031aa6be6c7c84be525b32c1
taktod <poepoemix@hotmail.com>
2014-02-08 08:47:50 +0900

    mkvのsimpleBlockの解析方法を記載

--- myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/SimpleBlock.java
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/SimpleBlock.java
@@ -6,6 +6,21 @@ import com.ttProject.unit.extra.EbmlValue;
 
 /**
  * SimpleBlockタグ
+ * データは次のようになっています。
+ * A3 44 B4 81 00 00 80 00 00 02 6C ...
+ *  A3[SimpleBlockタグ]
+ *  44 B4[このTagのサイズデータ]
+ * ここまでは読み込み動作実装済み
+ *  81[EbmlValue] 動作トラックデータ
+ *  00 00[16bit固定]このCluster上でのtimestamp差分量
+ *  1000 0000
+ *  . keyFrameであるか指定
+ *   ... reserved0設定
+ *       . 非表示フレームであるか？ 1なら非表示
+ *        .. lacing設定(データが複数のフレームの塊の場合にどのようにわかれるかの指定がはいっている*1)
+ *          . discardable:なんだろう？
+ * *1:h264のnalはフレームの塊ではあるけど、lacingではなくnal構造で分かれるようになっています。
+ * @see http://matroska.org/technical/specs/index.html#simpleblock_structure
  * @author taktod
  */
 public class SimpleBlock extends MkvBinaryTag {
