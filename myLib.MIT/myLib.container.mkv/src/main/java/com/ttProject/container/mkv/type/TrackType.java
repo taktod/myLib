804cea272898e8ba283d2c5c42e81f88f90750cb
taktod <poepoemix@hotmail.com>
2014-02-10 20:31:19 +0900

    データがなにであるか判定にMediaというenumをつくってみた。

--- myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/TrackType.java
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/TrackType.java
@@ -22,4 +22,27 @@ public class TrackType extends MkvUnsignedIntTag {
 	@Override
 	protected void requestUpdate() throws Exception {
 	}
+	public Media getType() throws Exception {
+		return Media.getType((int)super.getValue());
+	}
+	public static enum Media {
+		Video(0x01),
+		Audio(0x02),
+		Complex(0x03),
+		Logo(0x10),
+		Subtitle(0x11),
+		Buttons(0x20);
+		private final int value;
+		private Media(int value) {
+			this.value = value;
+		}
+		public static Media getType(int value) throws Exception {
+			for(Media t : values()) {
+				if(t.value == value) {
+					return t;
+				}
+			}
+			throw new Exception("typeが決定しませんでした:" + value);
+		}
+	}
 }
