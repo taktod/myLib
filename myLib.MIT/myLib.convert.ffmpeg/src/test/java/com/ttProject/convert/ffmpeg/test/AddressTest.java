8e966fb307dcca62edaaac716399d13f616c9c8c
taktod <poepoemix@hotmail.com>
2014-02-11 22:59:17 +0900

    addressがどうなっているか確認用のコード書いた。

---
+++ myLib.MIT/myLib.convert.ffmpeg/src/test/java/com/ttProject/convert/ffmpeg/test/AddressTest.java
@@ -0,0 +1,12 @@
+package com.ttProject.convert.ffmpeg.test;
+
+import java.net.InetSocketAddress;
+
+import org.junit.Test;
+
+public class AddressTest {
+	@Test
+	public void test() throws Exception {
+		System.out.println(new InetSocketAddress(1935));
+	}
+}
