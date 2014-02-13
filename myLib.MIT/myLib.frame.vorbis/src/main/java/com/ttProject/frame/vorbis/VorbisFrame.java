509a2f438b580706f2b0bc34224a80679b5383de
taktod <poepoemix@hotmail.com>
2014-02-13 20:02:35 +0900

    speexと同じ手でidentificationHeaderFrameを扱えるようにしておく。

--- myLib.MIT/myLib.frame.vorbis/src/main/java/com/ttProject/frame/vorbis/VorbisFrame.java
+++ myLib.MIT/myLib.frame.vorbis/src/main/java/com/ttProject/frame/vorbis/VorbisFrame.java
@@ -1,6 +1,7 @@
 package com.ttProject.frame.vorbis;
 
 import com.ttProject.frame.AudioFrame;
+import com.ttProject.frame.vorbis.type.IdentificationHeaderFrame;
 
 /**
  * vorbisのframe
@@ -14,4 +15,20 @@ import com.ttProject.frame.AudioFrame;
  * で、データ部だけ、xuggleのframeとしてほしいところ。
  */
 public abstract class VorbisFrame extends AudioFrame {
+	/** データ参照用のIdentificationHeaderFrame */
+	private IdentificationHeaderFrame identificationHeaderFrame = null;
+	/**
+	 * identificationHeaderFrame(情報を保持している)を設定
+	 * @param headerFrame
+	 */
+	public void setIdentificationHeaderFrame(IdentificationHeaderFrame headerFrame) {
+		this.identificationHeaderFrame = headerFrame;
+	}
+	/**
+	 * identificationHeaderFrameを参照
+	 * @return
+	 */
+	protected IdentificationHeaderFrame getHeaderFrame() {
+		return identificationHeaderFrame;
+	}
 }
