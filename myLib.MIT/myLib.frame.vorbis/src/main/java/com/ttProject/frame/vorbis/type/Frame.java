daddec696bc80aa60c3245be9aef66baf449697b
taktod <poepoemix@hotmail.com>
2014-02-13 21:52:02 +0900

    データ実体を保持するように調整した。

--- myLib.MIT/myLib.frame.vorbis/src/main/java/com/ttProject/frame/vorbis/type/Frame.java
+++ myLib.MIT/myLib.frame.vorbis/src/main/java/com/ttProject/frame/vorbis/type/Frame.java
@@ -4,26 +4,38 @@ import java.nio.ByteBuffer;
 
 import com.ttProject.frame.vorbis.VorbisFrame;
 import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.util.BufferUtil;
 
+/**
+ * vorbisの通常フレーム
+ * @author taktod
+ */
 public class Frame extends VorbisFrame {
-
+	private ByteBuffer buffer = null;
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void minimumLoad(IReadChannel channel) throws Exception {
-		// TODO Auto-generated method stub
-
+		super.setSize(channel.size());
 	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void load(IReadChannel channel) throws Exception {
-		// TODO Auto-generated method stub
-
+		buffer = BufferUtil.safeRead(channel, channel.size());
 	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected void requestUpdate() throws Exception {
-		// TODO Auto-generated method stub
 
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public ByteBuffer getPackBuffer() {
 		return null;
