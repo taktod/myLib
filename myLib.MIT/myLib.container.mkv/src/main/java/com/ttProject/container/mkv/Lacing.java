35ee5aedcbb770e5da81e6660ceaf8b3613dcb4d
taktod <poepoemix@hotmail.com>
2014-02-19 06:13:22 +0900

    lacingの解析用動作を追加

---
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/Lacing.java
@@ -0,0 +1,27 @@
+package com.ttProject.container.mkv;
+
+/**
+ * lacingのデータ用enum
+ * @author taktod
+ */
+public enum Lacing {
+	No(0),
+	Xiph(1),
+	EBML(3),
+	FixedSize(2);
+	private int value;
+	private Lacing(int value) {
+		this.value = value;
+	}
+	public int intValue() {
+		return value;
+	}
+	public Lacing getType(int value) throws Exception {
+		for(Lacing t : values()) {
+			if(t.value == value) {
+				return t;
+			}
+		}
+		throw new Exception("lacingデータが解析できませんでした。:" + value);
+	}
+}
