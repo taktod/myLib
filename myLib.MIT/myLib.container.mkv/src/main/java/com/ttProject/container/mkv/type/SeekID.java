2bb183f9c4e5ad1b98e1e52bda09529df763a162
taktod <poepoemix@hotmail.com>
2014-02-25 20:30:38 +0900

    SeekIdがさしているTagのデータを応答するようにしてみた。

--- myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/SeekID.java
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/SeekID.java
@@ -2,6 +2,9 @@ package com.ttProject.container.mkv.type;
 
 import com.ttProject.container.mkv.MkvBinaryTag;
 import com.ttProject.container.mkv.Type;
+import com.ttProject.nio.channels.ByteReadChannel;
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.unit.extra.BitLoader;
 import com.ttProject.unit.extra.EbmlValue;
 
 /**
@@ -9,6 +12,7 @@ import com.ttProject.unit.extra.EbmlValue;
  * @author taktod
  */
 public class SeekID extends MkvBinaryTag {
+	private Type targetTag = null;
 	/**
 	 * コンストラクタ
 	 * @param size
@@ -22,5 +26,36 @@ public class SeekID extends MkvBinaryTag {
 	@Override
 	protected void requestUpdate() throws Exception {
 		
+	}
+	/**
+	 * seek先のIdを参照したい。
+	 * @return
+	 * @throws Exception
+	 */
+	public Type getId() throws Exception {
+		if(targetTag == null) {
+			IReadChannel channel = null;
+			channel = new ByteReadChannel(getMkvData());
+			BitLoader loader = new BitLoader(channel);
+			EbmlValue value = new EbmlValue();
+			loader.load(value);
+			targetTag = Type.getType(value.getEbmlValue());
+		}
+		return targetTag;
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString(String space) {
+		StringBuilder data = new StringBuilder();
+		data.append(super.toString(space));
+		try {
+			data.append(" seekId:").append(getId());
+		}
+		catch(Exception e) {
+			data.append(" seekId: unknown.");
+		}
+		return data.toString();
 	}
 }
