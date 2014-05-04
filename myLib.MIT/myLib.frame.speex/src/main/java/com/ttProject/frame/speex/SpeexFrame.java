4497b58b54efbbc8e0d6b9bb1b20339379d320e4
taktod <poepoemix@hotmail.com>
2014-05-04 13:25:40 +0900

    speexもつくった。

--- myLib.MIT/myLib.frame.speex/src/main/java/com/ttProject/frame/speex/SpeexFrame.java
+++ myLib.MIT/myLib.frame.speex/src/main/java/com/ttProject/frame/speex/SpeexFrame.java
@@ -1,7 +1,11 @@
 package com.ttProject.frame.speex;
 
 import com.ttProject.frame.AudioFrame;
+import com.ttProject.frame.speex.type.Frame;
 import com.ttProject.frame.speex.type.HeaderFrame;
+import com.ttProject.nio.channels.ByteReadChannel;
+import com.ttProject.nio.channels.IReadChannel;
+import com.ttProject.util.HexUtil;
 
 /**
  * speexのframe
@@ -29,4 +33,34 @@ public abstract class SpeexFrame extends AudioFrame {
 		return headerFrame;
 	}
 	public abstract boolean isComplete();
+	/**
+	 * 無音frameのサンプルを応答します。
+	 * @param sampleRate サンプルレート
+	 * @param channels チャンネル数
+	 * @param bitSize mp3には関係なし
+	 * @return
+	 */
+	public static Frame getMutedFrame(int sampleRate, int channels, int bitSize) throws Exception {
+		String bufferString = null;
+		if(channels != 1) {
+			throw new RuntimeException("チャンネル設定が想定外でした。");
+		}
+		switch(sampleRate) {
+		case 16000:
+			bufferString = "3E9D1B9A2008017FFFFFFFFFFFDB6DB6DB6DB68400BFFFFFFFFFFFEDB6DB6DB6DB42005FFFFFFFFFFFF6DB6DB6DB6DA1002FFFFFFFFFFFFB6DB6DB6DB6DC3B60ABABABABABABABABABAB0ABABABABABABABABABAB0ABABABABABABABABABAB0ABABABABABABABABABAB7";
+			break;
+		default:
+			throw new RuntimeException("想定外でした。");
+		}
+		IReadChannel channel = new ByteReadChannel(HexUtil.makeBuffer(bufferString));
+		Frame frame = new Frame();
+		// あとでsetHeaderFrameしておいてほしいところ。
+		// frame.setHeaderFrame(headerFrame);
+		frame.minimumLoad(channel);
+		frame.load(channel);
+		frame.setChannel(channels);
+		frame.setBit(16);
+		frame.setSampleRate(sampleRate);
+		return frame;
+	}
 }
---
+++ myLib.MIT/myLib.frame.speex/src/test/java/com/ttProject/frame/speex/test/MutedFrameTest.java
@@ -0,0 +1,26 @@
+package com.ttProject.frame.speex.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.ttProject.frame.speex.SpeexFrame;
+import com.ttProject.frame.speex.type.Frame;
+import com.ttProject.frame.speex.type.HeaderFrame;
+import com.ttProject.util.HexUtil;
+
+/**
+ * 無音frameの動作テスト
+ * @author taktod
+ */
+public class MutedFrameTest {
+	/** ロガー */
+	private Logger logger = Logger.getLogger(MutedFrameTest.class);
+	@Test
+	public void test() throws Exception {
+		Frame frame = SpeexFrame.getMutedFrame(16000, 1, 16);
+		HeaderFrame headerFrame = new HeaderFrame();
+		headerFrame.fillWithFlvDefault();
+		frame.setHeaderFrame(headerFrame);
+		logger.info(HexUtil.toHex(frame.getData()));
+	}
+}
