1e752382d8b660d0c4bddee05de0a9d536011386
taktod <poepoemix@hotmail.com>
2014-05-24 12:49:54 +0900

    ライセンス情報をjavaファイルの先頭に書き込みするプログラムを書いてみた。 どうかね？

---
+++ myLib.MIT/myLib.license/src/test/java/com/ttProject/license/WriteTest.java
@@ -0,0 +1,106 @@
+/*
+ * myLib - https://github.com/taktod/myLib
+ * Copyright (c) 2014 ttProject. All rights reserved.
+ * 
+ * Licensed under The MIT license.
+ */
+package com.ttProject.license;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * ライセンス記述を記載するプログラム
+ * @author taktod
+ */
+public class WriteTest {
+	/** ロガー */
+	private Logger logger = Logger.getLogger(WriteTest.class);
+	/**
+	 * javaファイルの先頭にライセンス記述を書き込むプログラム
+	 */
+	@Test
+	public void javaDoc() {
+		logger.info("処理テスト開始");
+		// まずファイルをサーチする必要あり。
+		File dir = new File("../myLib.channels");
+		searchDir(dir);
+		logger.info("処理テストおわり");
+	}
+	private void searchDir(File dir) {
+		if(!dir.isDirectory()) {
+			checkJava(dir);
+			return;
+		}
+		for(File f : dir.listFiles()) {
+			searchDir(f);
+		}
+	}
+	private void checkJava(File f) {
+		if(!f.getName().endsWith(".java")) {
+			return;
+		}
+		else {
+			logger.info(f);
+			writeLicense(f);
+		}
+	}
+	private void writeLicense(File f) {
+		// ファイルを確認して、先頭がライセンスでなければライセンス条文を挿入する必要あり。
+		String license = "/*\n" + 
+" * myLib - https://github.com/taktod/myLib\n" + 
+" * Copyright (c) 2014 ttProject. All rights reserved.\n" + 
+" * \n" + 
+" * Licensed under The MIT license.\n" + 
+" */\n";
+		FileOutputStream fos = null;
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(f));
+			String firstLine = br.readLine();
+			if(firstLine.equals("/*")){
+				// すでにライセンスが入力済み
+				return;
+			}
+			// ライセンスがはいっていないので、いれる必要あり。
+			File ftmp = new File(f.getAbsolutePath() + "tmp");
+			fos = new FileOutputStream(f.getAbsolutePath() + "tmp");
+			fos.write(license.getBytes());
+			fos.write((firstLine + "\n").getBytes());
+			String line = null;
+			while((line = br.readLine()) != null) {
+				fos.write((line + "\n").getBytes());
+			}
+			fos.close();
+			fos = null;
+			br.close();
+			br = null;
+			ftmp.renameTo(f);
+		}
+		catch(Exception e) {
+		}
+		finally {
+			if(fos != null) {
+				try {
+					fos.close();
+					fos = null;
+				}
+				catch(Exception e) {
+				}
+			}
+			if(br != null) {
+				try {
+					br.close();
+					br = null;
+				}
+				catch(Exception e) {
+				}
+			}
+		}
+	}
+}
