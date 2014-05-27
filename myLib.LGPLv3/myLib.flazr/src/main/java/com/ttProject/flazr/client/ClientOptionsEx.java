ee8a66340682412d19c9ae72848ae1a16014d1bc
taktod <poepoemix@hotmail.com>
2014-05-27 21:57:06 +0900

    コマンドラインで追加したobjectEncodingの値が文字列の3になってサーバーに送信されてしまうのが 気に入らないので、対処してみた。

---
+++ myLib.LGPLv3/myLib.flazr/src/main/java/com/ttProject/flazr/client/ClientOptionsEx.java
@@ -0,0 +1,33 @@
+/*
+ * myLib - https://github.com/taktod/myLib
+ * Copyright (c) 2014 ttProject. All rights reserved.
+ * 
+ * Licensed under GNU LESSER GENERAL PUBLIC LICENSE Version 3.
+ */
+package com.ttProject.flazr.client;
+
+import com.flazr.rtmp.client.ClientOptions;
+
+/**
+ * clientOptionsの動作を再定義しておく
+ * objectEncodingの部分だけ、値を文字列からNumberに変更しておきたい
+ * @author taktod
+ */
+public class ClientOptionsEx extends ClientOptions {
+	/**
+	 * url解析動作をhookしておく
+	 */
+	public boolean parseCli(String[] args) {
+		// 普通に解析しておく
+		boolean result = super.parseCli(args);
+		// objectEncodingの値だけ、数値である必要があるので(特にwowza)値を確認して数値化しておく。
+		Object encoding = getParams().get("objectEncoding");
+		if("3".equals(encoding)) {
+			putParam("objectEncoding", 3);
+		}
+		else {
+			putParam("objectEncoding", 0);
+		}
+		return result;
+	};
+}
