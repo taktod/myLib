4a0d41465dab12182b0585208aa6a014fc0f24c9
taktod <poepoemix@hotmail.com>
2014-07-25 18:31:45 +0900

    rangeOrderによる圧縮うまく動作しない、たぶん繰り上がりの計算がないからだと思う。 まぁ、なんとかしとこう。

--- myLib.MIT/myLib.frame.adpcmimawav/src/test/java/com/ttProject/frame/adpcmimawav/test/RangeCoderTest.java
+++ myLib.MIT/myLib.frame.adpcmimawav/src/test/java/com/ttProject/frame/adpcmimawav/test/RangeCoderTest.java
@@ -1,5 +1,6 @@
 package com.ttProject.frame.adpcmimawav.test;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,7 +24,7 @@ public class RangeCoderTest {
 	private Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
 	private int weight = 0;
 	{
-/*		map2.put(0x0, 4);
+		map2.put(0x0, 4);
 		map2.put(0x1, 4);
 		map2.put(0x2, 4);
 		map2.put(0x3, 3);
@@ -38,11 +39,11 @@ public class RangeCoderTest {
 		map2.put(0xC, 1);
 		map2.put(0xD, 1);
 		map2.put(0xE, 1);
-		map2.put(0xF, 1);*/
-		map2.put(0xA, 4);
+		map2.put(0xF, 1);
+/*		map2.put(0xA, 4);
 		map2.put(0xB, 2);
 		map2.put(0xC, 1);
-		map2.put(0xD, 1);
+		map2.put(0xD, 1);*/
 		for(Entry<Integer, Integer> entry : map2.entrySet()) {
 			map.put(entry.getKey(), weight);
 			weight += entry.getValue();
@@ -65,19 +66,19 @@ public class RangeCoderTest {
 	public void test() throws Exception {
 		logger.info("開始");
 		// とりあえず0x12345678123412341234123411111111を圧縮してみることにする。
-//		byte[] data = {0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x12, 0x34, 0x12, 0x34, 0x12, 0x34, 0x11, 0x11, 0x11, 0x11, 0x11};
-		byte[] data = {(byte)0xdc, (byte)0xbb, (byte)0xaa, (byte)0xaa, (byte)0xbb, (byte)0xaa, (byte)0xaa};
-		logger.info(HexUtil.toHex(data, true));
+		ByteBuffer data = HexUtil.makeBuffer("123456");
+		logger.info(HexUtil.toHex(data));
 		// range圧縮する。
 		// 始め
 		// [0, 0x01000000]
 		int low, range;
 		low = 0;
 		range = 0x01000000;
+		logger.info(Integer.toHexString(low) + " " + Integer.toHexString(range));
 		List<Byte> foundList = new ArrayList<Byte>();
-		for(byte b : data) {
+		while(data.remaining() > 0) {
+			byte b = data.get();
 			int bit = ((b & 0xFF) >>> 4);
-			logger.info(Integer.toHexString(bit));
 			low += range * map.get(bit) / weight;
 			range = range * map2.get(bit) / weight;
 			if(range < 0x010000) {
@@ -87,7 +88,6 @@ public class RangeCoderTest {
 			}
 			logger.info(Integer.toHexString(low) + " " + Integer.toHexString(range));
 			bit = (b & 0x0F);
-			logger.info(Integer.toHexString(bit));
 			low += range * map.get(bit) / weight;
 			range = range * map2.get(bit) / weight;
 			if(range < 0x010000) {
@@ -104,25 +104,22 @@ public class RangeCoderTest {
 		for(int i = 0;i < foundList.size();i ++) {
 			result[i] = foundList.get(i);
 		}
-		logger.info(HexUtil.toHex(result, true));
+		logger.info(HexUtil.toHex(result));
 	}
 	@Test
 	public void decodeTest() throws Exception {
 		logger.info("decode開始");
 		// FA 82 80 00 00
 //		byte[] data = {(byte)0xFA, (byte)0x80, (byte)0x00, (byte)0x00};
-		byte[] data = {(byte)0xFA, (byte)0x82, (byte)0x80, (byte)0x00, (byte)0x00};
-		List<Byte> list = new ArrayList<Byte>();
-		for(byte b : data) {
-			list.add(b);
-		}
+//		byte[] data = {(byte)0xFA, (byte)0x82, (byte)0x80, (byte)0x00, (byte)0x00};
+		ByteBuffer data = HexUtil.makeBuffer("219C20876B");
+//		byte[] data = {(byte)0x6B, (byte)0xCE, (byte)0xE8, (byte)0x96, (byte)0x68, (byte)0x40, (byte)0x70, (byte)0x0D};
 		int code, range;
 		code = 0;
-		int i = 0;
 		// とりあえず先頭の３つ読み込む
-		code = code << 8 | (list.get(i ++) & 0xFF);
-		code = code << 8 | (list.get(i ++) & 0xFF);
-		code = code << 8 | (list.get(i ++) & 0xFF);
+		code = code << 8 | (data.get() & 0xFF);
+		code = code << 8 | (data.get() & 0xFF);
+		code = code << 8 | (data.get() & 0xFF);
 		logger.info(Integer.toHexString(code));
 		range = 0x1000000;
 		while(true) {
@@ -132,19 +129,20 @@ public class RangeCoderTest {
 			for(Entry<Integer, Integer> entry : map.entrySet()) {
 				if(entry.getValue() >= pos || entry.getValue() + map2.get(entry.getKey()) > pos) {
 					val = entry.getKey();
-					logger.info(Integer.toHexString(val));
+					System.out.print(Integer.toHexString(val));
 					break;
 				}
 			}
 			code = code - range * map.get(val) / weight;
 			range = range * map2.get(val) / weight;
 			if(range < 0x010000) {
-				if(i == list.size()) {
+				if(data.remaining() == 0) {
 					break;
 				}
 				range = range * 0x0100;
-				code = code << 8 | (list.get(i ++) & 0xFF);
+				code = code << 8 | (data.get() & 0xFF);
 			}
 		}
+		System.out.println();
 	}
 }
