017c29d8ad9ef80c8999d26c6b605e1c6c2e68ba
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-07-28 10:26:09 +0900

    ConfigDataの処理内容を更新しました。 これでより正しいデータが取得できるようになります (が、こんな変なデータを送ってくるクライアントないと思う。)

--- myLib.MIT/myLib.frame.h264/src/main/java/com/ttProject/frame/h264/ConfigData.java
+++ myLib.MIT/myLib.frame.h264/src/main/java/com/ttProject/frame/h264/ConfigData.java
@@ -7,6 +7,8 @@
 package com.ttProject.frame.h264;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ttProject.frame.IVideoFrame;
 import com.ttProject.frame.extra.VideoMultiFrame;
@@ -15,6 +17,13 @@ import com.ttProject.frame.h264.type.SequenceParameterSet;
 import com.ttProject.nio.channels.ByteReadChannel;
 import com.ttProject.nio.channels.IReadChannel;
 import com.ttProject.unit.ISelector;
+import com.ttProject.unit.extra.BitLoader;
+import com.ttProject.unit.extra.bit.Bit16;
+import com.ttProject.unit.extra.bit.Bit2;
+import com.ttProject.unit.extra.bit.Bit3;
+import com.ttProject.unit.extra.bit.Bit5;
+import com.ttProject.unit.extra.bit.Bit6;
+import com.ttProject.unit.extra.bit.Bit8;
 import com.ttProject.util.BufferUtil;
 
 /**
@@ -37,7 +46,7 @@ import com.ttProject.util.BufferUtil;
  * 01 4D 40 1E FF E1 00 19 67 4D 40 1E 92 42 01 40 5F F2 E0 22 00 00 03 00 C8 00 00 2E D5 1E 2C 5C 90 01 00 04 68 EE 32 C8
  * [] avcC version1(互換性がくずれたら新しい番号が振られるらしい、いまのところ1以外みたことない)
  *    [      ] profile compatibility levelの３点セット
- *             [] legthSizeMinusOneWithReserved 0x3F | (nalLength - 1); (nalLengthは1,2,4のどれかっぽい)
+ *             [] lengthSizeMinusOneWithReserved 0x3F | (nalLength - 1); (nalLengthは1,2,4のどれかっぽい)
  *                                      ここ・・・0xFCの間違いじゃないかな (注)
  *                [] numOfSequenceParameterSetsWithReserved 0xE0 | 数になる(この場合１つ)
  *                   [   ] spsの長さ
@@ -53,17 +62,113 @@ import com.ttProject.util.BufferUtil;
  * 09 00 03 05 00 00 00 00 00 00 00 17 01 00 00 00 00 00 02 fc 65 88 80 80  0f ff 
  *                                                 [         ]この部分0x000002fcがNalのサイズになっていますが、
  *                                                 これはNalLengthMinusOneが3になっているので4byteになっているだけみたいです。
+ * mkvやmp4、flvでlengthSizeMinusOneの部分が3の場合に4byteによるnalデータサイズ定義があることを確認しました。
+ * これらのコンテナでは、ConfigDataを取り回して使わないとNalのサイズ決定ができないっぽいですね。
  * @author taktod
  */
 public class ConfigData {
 	/** 動作セレクター(セレクターにsps ppsを保持させて、他のframeに持たせる必要があるので、ここで設置できるようにしてある) */
 	private H264FrameSelector selector = null;
+	private Bit8 avcCVersion = new Bit8();
+	private Bit8 profile         = new Bit8();
+	private Bit8 compatibility   = new Bit8();
+	private Bit8 level           = new Bit8();
+	private Bit6 reservedBit1    = new Bit6();
+	private Bit2 nalSizeMinusOne = new Bit2();
+
+	private Bit3 reservedBit2    = new Bit3();
+	private Bit5 numOfSps = new Bit5();
+	private List<SequenceParameterSet> spsList = new ArrayList<SequenceParameterSet>();
+
+	private Bit8 numOfPps = new Bit8();
+	private List<PictureParameterSet> ppsList = new ArrayList<PictureParameterSet>();
+	// 以下SpsExt(profile = 100 110 122 144でのみ必須)
+/*		private Bit6 reservedBit3;
+		private Bit2 chromaFormat;
+		private Bit5 reservedBit4;
+		private Bit3 bitDepthLumaMinus8;
+		private Bit5 reservedBit5;
+		private Bit3 bitDepthChromaMinus8;
+		private Bit8 numOfSpse;
+		private List<SequenceParameterSetExt> spseList; */
 	/**
 	 * セレクターの設定
 	 * @param selector
 	 */
 	public void setSelector(H264FrameSelector selector) {
 		this.selector = selector;
+	}
+	/**
+	 * nalSizeのデータを参照
+	 * @return
+	 */
+	public int getNalSizeBytes() {
+		return nalSizeMinusOne.get() + 1;
+	}
+	/**
+	 * spsのリストを応答する
+	 * @return
+	 */
+	public List<SequenceParameterSet> getSpsList() {
+		return new ArrayList<SequenceParameterSet>(spsList);
+	}
+	/**
+	 * ppsのリストを応答する
+	 * @return
+	 */
+	public List<PictureParameterSet> getPpsList() {
+		return new ArrayList<PictureParameterSet>(ppsList);
+	}
+	/**
+	 * configDataを解析します。
+	 * @param channel
+	 * @throws Exception
+	 */
+	public void analyzeData(IReadChannel channel) throws Exception {
+		// データを解析します。
+		ISelector selector = null;
+		if(this.selector != null) {
+			selector = this.selector;
+		}
+		else {
+			selector = new H264FrameSelector();
+		}
+		// まず先頭の5byteを読み込む
+		BitLoader loader = new BitLoader(channel);
+		loader.load(avcCVersion, profile, compatibility, level, reservedBit1, nalSizeMinusOne);
+		// 続いてspsの数を取得しておく。
+		loader.load(reservedBit2, numOfSps);
+		for(int i = 0;i < numOfSps.get();i ++) {
+			Bit16 spsSize = new Bit16();
+			loader.load(spsSize);
+			IReadChannel byteChannel = new ByteReadChannel(BufferUtil.safeRead(channel, spsSize.get()));
+			IVideoFrame sps = (IVideoFrame)selector.select(byteChannel);
+			if(!(sps instanceof SequenceParameterSet)) {
+				throw new Exception("spsがくるべきところでspsが取得できませんでした。");
+			}
+			spsList.add((SequenceParameterSet)sps);
+		}
+		// 続いてppsの数を取得
+		loader.load(numOfPps);
+		for(int i = 0;i < numOfPps.get();i ++) {
+			Bit16 ppsSize = new Bit16();
+			loader.load(ppsSize);
+			IReadChannel byteChannel = new ByteReadChannel(BufferUtil.safeRead(channel, ppsSize.get()));
+			IVideoFrame pps = (IVideoFrame)selector.select(byteChannel);
+			if(!(pps instanceof PictureParameterSet)) {
+				throw new Exception("ppsがくるべきところでppsが取得できませんでした。");
+			}
+			ppsList.add((PictureParameterSet)pps);
+		}
+		switch(profile.get()) {
+		case 100:
+		case 110:
+		case 122:
+		case 144:
+			throw new Exception("spsExtを読み込まないとだめなデータについては、未実装です。");
+		default:
+			break;
+		}
 	}
 	/**
 	 * spsとppsを取り出す
--- myLib.MIT/myLib.frame.h264/src/test/java/com/ttProject/frame/h264/test/ConfigReadTest.java
+++ myLib.MIT/myLib.frame.h264/src/test/java/com/ttProject/frame/h264/test/ConfigReadTest.java
@@ -15,15 +15,32 @@ import com.ttProject.nio.channels.ByteReadChannel;
 import com.ttProject.nio.channels.IReadChannel;
 import com.ttProject.util.HexUtil;
 
+/**
+ * configDataの読み込み動作テスト
+ * @author taktod
+ */
 public class ConfigReadTest {
+	/** ロガー */
 	private Logger logger = Logger.getLogger(ConfigReadTest.class);
-	@Test
-	public void test() throws Exception {
+//	@Test
+	public void test_old() throws Exception {
 		IReadChannel channel = new ByteReadChannel(HexUtil.makeBuffer("014D401EFFE10019674D401E924201405FF2E02200000300C800002ED51E2C5C9001000468EE32C8"));
 		// channelのデータを読み込んでpspとppsが取得できれば御の字
 		ConfigData cdata = new ConfigData();
 		IVideoFrame frame = cdata.getNalsFrame(channel);
 		logger.info(frame.getWidth());
 		logger.info(frame.getHeight());
+	}
+	@Test
+	public void test() throws Exception {
+		IReadChannel channel = new ByteReadChannel(HexUtil.makeBuffer("014D401EFFE10019674D401E924201405FF2E02200000300C800002ED51E2C5C9001000468EE32C8"));
+		// channelのデータを読み込んでpspとppsが取得できれば御の字
+		ConfigData cData = new ConfigData();
+		cData.analyzeData(channel);
+		logger.info(cData.getSpsList());
+		logger.info(cData.getPpsList());
+		logger.info(cData.getNalSizeBytes());
+		logger.info(cData.getSpsList().get(0).getWidth());
+		logger.info(cData.getSpsList().get(0).getHeight());
 	}
 }
