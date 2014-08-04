8c47d6d6a2575a850b65cc52e0a28d82d39246ee
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-08-04 11:29:14 +0900

    いろいろな動作をさせるためのentry

---
+++ myLib.MIT/myLib.convertprocess/src/main/java/com/ttProject/convertprocess/process/FlvOutputEntry.java
@@ -0,0 +1,15 @@
+/*
+ * myLib - https://github.com/taktod/myLib
+ * Copyright (c) 2014 ttProject. All rights reserved.
+ * 
+ * Licensed under The MIT license.
+ */
+package com.ttProject.convertprocess.process;
+
+/**
+ * 通常のflvとしてffmpegにデータを渡すentry
+ * @author taktod
+ */
+public class FlvOutputEntry {
+
+}
---
+++ myLib.MIT/myLib.convertprocess/src/main/java/com/ttProject/convertprocess/process/FlvVideoOutputEntry.java
@@ -0,0 +1,15 @@
+/*
+ * myLib - https://github.com/taktod/myLib
+ * Copyright (c) 2014 ttProject. All rights reserved.
+ * 
+ * Licensed under The MIT license.
+ */
+package com.ttProject.convertprocess.process;
+
+/**
+ * flvの映像の部分のみ取り出したデータとして、ffmpegにデータを渡すentry
+ * @author taktod
+ */
+public class FlvVideoOutputEntry {
+
+}
