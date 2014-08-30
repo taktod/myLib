05dc586b4990d0920fc243dd4d8b8226e9950a9d
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-08-30 20:40:12 +0900

    riffCodecTypeも調整しといた。

--- myLib.MIT/myLib.container.riff/src/main/java/com/ttProject/container/riff/RiffCodecType.java
+++ myLib.MIT/myLib.container.riff/src/main/java/com/ttProject/container/riff/RiffCodecType.java
@@ -6,27 +6,34 @@
  */
 package com.ttProject.container.riff;
 
+import com.ttProject.frame.CodecType;
+
 /**
  * コーデック情報
  * いろいろあるけど、とりあえず一部だけいれとく。
  * @author taktod
  */
 public enum RiffCodecType {
-	Unknown(0x0000),
-	PCM(0x0001),
-	MS_ADPCM(0x0002),
-	IBM_CSVD(0x0005),
-	A_LAW(0x0006),
-	U_LAW(0x0007),
-	OKI_ADPCM(0x0010),
-	IMA_ADPCM(0x0011), // 今回の肝
-	MP3(0x0055);
+	Unknown(0x0000, CodecType.NONE),
+	PCM(0x0001, CodecType.UNKNOWN_AUDIO),
+	MS_ADPCM(0x0002, CodecType.UNKNOWN_AUDIO),
+	IBM_CSVD(0x0005, CodecType.UNKNOWN_AUDIO),
+	A_LAW(0x0006, CodecType.PCM_ALAW),
+	U_LAW(0x0007, CodecType.PCM_MULAW),
+	OKI_ADPCM(0x0010, CodecType.UNKNOWN_AUDIO),
+	IMA_ADPCM(0x0011, CodecType.ADPCM_IMA_WAV), // 今回の肝
+	MP3(0x0055, CodecType.MP3);
 	private final int value;
-	private RiffCodecType(int value) {
+	private final CodecType codecType;
+	private RiffCodecType(int value, CodecType codecType) {
 		this.value = value;
+		this.codecType = codecType;
 	}
 	public int intValue() {
 		return value;
+	}
+	public CodecType getCodecType() {
+		return codecType;
 	}
 	public static RiffCodecType getCodec(int num) {
 		for(RiffCodecType type : values()) {
