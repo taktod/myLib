08867ac12744f5662b8fecb507478a7e8d6e6824
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-09-05 19:20:45 +0900

    Seekもいっきに設定可能にしてみた。

--- myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/Seek.java
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/Seek.java
@@ -6,6 +6,8 @@
  */
 package com.ttProject.container.mkv.type;
 
+import java.nio.ByteBuffer;
+
 import com.ttProject.container.mkv.MkvMasterTag;
 import com.ttProject.container.mkv.Type;
 import com.ttProject.unit.extra.EbmlValue;
@@ -27,5 +29,22 @@ public class Seek extends MkvMasterTag {
 	 */
 	public Seek() {
 		this(new EbmlValue());
+	}
+	/**
+	 * seek情報を作り上げておく
+	 * @param type
+	 * @param pos
+	 * @throws Exception
+	 */
+	public void setup(Type type, long pos) throws Exception {
+		SeekID seekId = new SeekID();
+		ByteBuffer idBuffer = ByteBuffer.allocate(4);
+		idBuffer.putInt(type.intValue());
+		idBuffer.flip();
+		seekId.setValue(idBuffer);
+		addChild(seekId);
+		SeekPosition position = new SeekPosition();
+		position.setValue(pos);
+		addChild(position);
 	}
 }
