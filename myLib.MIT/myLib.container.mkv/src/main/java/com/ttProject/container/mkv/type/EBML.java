5a6ea5fb0a799dda9ef098186442f1305e5f6e76
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-09-05 19:17:25 +0900

    ebmlの構築をmasterTag側で実施できるように調整。

--- myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/EBML.java
+++ myLib.MIT/myLib.container.mkv/src/main/java/com/ttProject/container/mkv/type/EBML.java
@@ -28,4 +28,41 @@ public class EBML extends MkvMasterTag {
 	public EBML() {
 		super(Type.EBML, new EbmlValue());
 	}
+	/**
+	 * 書き込み用のセットアップ処理
+	 * @param version
+	 * @param readVersion
+	 * @param docType
+	 * @param docTypeVersion
+	 * @param docTypeReadVersion
+	 */
+	public void setup(int version, int readVersion, String dType , int dTypeVersion, int dTypeReadVersion) throws Exception {
+		EBMLVersion ebmlVersion = new EBMLVersion();
+		ebmlVersion.setValue(version);
+		addChild(ebmlVersion);
+		
+		EBMLReadVersion ebmlReadVersion = new EBMLReadVersion();
+		ebmlReadVersion.setValue(readVersion);
+		addChild(ebmlReadVersion);
+		
+		EBMLMaxIDLength ebmlMaxIDLength = new EBMLMaxIDLength();
+		ebmlMaxIDLength.setValue(4);
+		addChild(ebmlMaxIDLength);
+		
+		EBMLMaxSizeLength ebmlMaxSizeLength = new EBMLMaxSizeLength();
+		ebmlMaxSizeLength.setValue(8);
+		addChild(ebmlMaxSizeLength);
+		
+		DocType docType = new DocType();
+		docType.setValue(dType);
+		addChild(docType);
+		
+		DocTypeVersion docTypeVersion = new DocTypeVersion();
+		docTypeVersion.setValue(dTypeVersion);
+		addChild(docTypeVersion);
+		
+		DocTypeReadVersion docTypeReadVersion = new DocTypeReadVersion();
+		docTypeReadVersion.setValue(dTypeReadVersion);
+		addChild(docTypeReadVersion);
+	}
 }
