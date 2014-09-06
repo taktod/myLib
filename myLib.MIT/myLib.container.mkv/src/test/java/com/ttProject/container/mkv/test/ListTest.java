41646f4422d768d441c0e13711195f092bc072f0
todatakahiko <toda.takahiko@hpsys.co.jp>
2014-09-06 09:03:27 +0900

    intValue化するとどうなるかテスト

--- myLib.MIT/myLib.container.mkv/src/test/java/com/ttProject/container/mkv/test/ListTest.java
+++ myLib.MIT/myLib.container.mkv/src/test/java/com/ttProject/container/mkv/test/ListTest.java
@@ -26,7 +26,7 @@ public class ListTest {
 		list.add(5);
 		list.add(2);
 		logger.info(list);
-		logger.info(list.remove((Integer)1));
+		logger.info(list.remove(((Integer)1).intValue()));
 		logger.info(list);
 	}
 }
