7fc67cee4fa442e62d4efe2b20c135a9b6c8c9e5
toda.takahiko@hps <toda.takahiko@hpsys.co.jp>
2014-10-09 16:56:41 +0900

    commit the test code. I will make avi reading for ffmpeg output.

---
+++ myLib.MIT/myLib.container.avi/src/test/java/com/ttProject/container/avi/test/AviTest.java
@@ -0,0 +1,49 @@
+package com.ttProject.container.avi.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.ttProject.container.IContainer;
+import com.ttProject.container.IReader;
+import com.ttProject.container.riff.RiffUnitReader;
+import com.ttProject.nio.channels.FileReadChannel;
+import com.ttProject.nio.channels.IFileReadChannel;
+
+/**
+ * avi load test
+ * @author taktod
+ */
+public class AviTest {
+	/** logger */
+	private Logger logger = Logger.getLogger(AviTest.class);
+	/**
+	 * test
+	 */
+	@Test
+	public void test() {
+		IFileReadChannel source = null;
+		try {
+			source = FileReadChannel.openFileReadChannel(
+					Thread.currentThread().getContextClassLoader().getResource("test.mjpeg.avi")
+			);
+			IReader reader = new RiffUnitReader();
+			IContainer container = null;
+			while((container = reader.read(source)) != null) {
+				logger.info(container);
+			}
+		}
+		catch(Exception e) {
+			logger.warn(e);
+		}
+		finally {
+			if(source != null) {
+				try {
+					source.close();
+				}
+				catch(Exception e) {
+				}
+				source = null;
+			}
+		}
+	}
+}
